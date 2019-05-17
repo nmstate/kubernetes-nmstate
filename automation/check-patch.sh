7d0d71b54789e63b6a212106b2778b9e4052176e
Petr Horacek <phoracek@redhat.com>
2019-05-17 13:58:17 +0200

    run functional tests on std-ci
    Signed-off-by: Petr Horacek <phoracek@redhat.com>

--- automation/check-patch.sh
+++ automation/check-patch.sh
@@ -1,8 +1,37 @@
 #!/bin/bash -xe
 
-
 main() {
-    echo "TODO: Add functional tests"
+    TARGET="$0"
+    TARGET="${TARGET#./}"
+    TARGET="${TARGET%.*}"
+    TARGET="${TARGET#*.}"
+    echo "TARGET=$TARGET"
+    export TARGET
+
+    echo "Setup Go paths"
+    cd ..
+    export GOROOT=/usr/local/go
+    export GOPATH=$(pwd)/go
+    export PATH=$GOPATH/bin:$GOROOT/bin:$PATH
+    mkdir -p $GOPATH
+
+    echo "Install Go 1.10"
+    export GIMME_GO_VERSION=1.10
+    mkdir -p /gimme
+    curl -sL https://raw.githubusercontent.com/travis-ci/gimme/master/gimme | HOME=/gimme bash >> /etc/profile.d/gimme.sh
+    source /etc/profile.d/gimme.sh
+
+    echo "Install testing dependencies"
+    go get github.com/onsi/ginkgo/ginkgo
+
+    echo "Install nmstate repository to the right place"
+    mkdir -p $GOPATH/src/github.com/nmstate
+    mkdir -p $GOPATH/pkg
+    ln -s $(pwd)/kubernetes-nmstate $GOPATH/src/github.com/nmstate/
+    cd $GOPATH/src/github.com/nmstate/kubernetes-nmstate
+
+    echo "Run functional tests"
+    exec automation/test.sh
 }
 
 [[ "${BASH_SOURCE[0]}" == "$0" ]] && main "$@"
