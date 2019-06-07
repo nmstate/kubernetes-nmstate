c59662275d2105be59e8c592e7668db00e87d6e3
Quique Llorente <quique.llorente@gmail.com>
2019-06-07 08:06:39 +0200

    Add minimal tracer bullet that creates NodeNetworkState
    It will just Reconcile at corev1.Node events, creating the
    NodeNetworkState if does not exists for that node and update
    status with harcoded yaml "interfaces: []"
    
    It included a e2e test there are two ways to run them:
    - out of cluster (fast iteration there is not docker involve)
      make cluster-up
      # To iterate
      make test/local/e2e
    
    - in cluster (slower but you test manifests)
      make cluster-up
      # To iterate manger and resources
      make cluster-sync test/cluster/e2e
      # To iterate manger and resources
      make cluster-sync-resources test/cluster/e2e
      # To iterate manger and resources
      make cluster-sync-manager test/cluster/e2e
    
    Signed-off-by: Quique Llorente <quique.llorente@gmail.com>

--- Makefile
+++ Makefile
@@ -1,11 +1,24 @@
 IMAGE_REGISTRY ?= quay.io
 IMAGE_REPO ?= nmstate
+
 MANAGER_IMAGE_NAME ?= kubernetes-nmstate-manager
 MANAGER_IMAGE_TAG ?= latest
-MANAGER_IMAGE_FULL_NAME ?= $(IMAGE_REGISTRY)/$(IMAGE_REPO)/$(MANAGER_IMAGE_NAME):$(MANAGER_IMAGE_TAG)
+MANAGER_IMAGE_FULL_NAME ?= $(IMAGE_REPO)/$(MANAGER_IMAGE_NAME):$(MANAGER_IMAGE_TAG)
+MANAGER_IMAGE ?= $(IMAGE_REGISTRY)/$(MANAGER_IMAGE_FULL_NAME)
+
 GINKGO_EXTRA_ARGS ?=
 GINKGO_ARGS ?= -v -r --randomizeAllSpecs --randomizeSuites --race --trace $(GINKGO_EXTRA_ARGS)
 GINKGO?= go run ./vendor/github.com/onsi/ginkgo/ginkgo
+
+KUBECONFIG ?= ./cluster/.kubeconfig
+# TODO: use operator-sdk from vendor/
+OPERATOR_SDK ?= operator-sdk
+LOCAL_REGISTRY ?= registry:5000
+KUBECTL ?= ./cluster/kubectl.sh
+
+local_manager_manifest = build/_output/manager.local.yaml
+
+resources = deploy/service_account.yaml deploy/role.yaml deploy/role_binding.yaml
 
 all: check manager
 
@@ -18,16 +31,41 @@ vet:
 	go vet ./cmd/... ./pkg/...
 
 manager:
-	operator-sdk build $(MANAGER_IMAGE_FULL_NAME)
+	$(OPERATOR_SDK) build $(MANAGER_IMAGE)
 
-push-manager:
-	docker push $(MANAGER_IMAGE_FULL_NAME)
+manager-up:
+	$(KUBECTL) apply -f deploy/crds/nmstate_v1_nodenetworkstate_crd.yaml
+	OPERATOR_NAME=kubernetes-nmstate \
+				  $(OPERATOR_SDK) up local --kubeconfig $(KUBECONFIG)
 
 gen-k8s:
-	operator-sdk generate k8s
+	$(OPERATOR_SDK) generate k8s
+
+push-manager: manager
+	docker push $(MANAGER_IMAGE)
 
 unit-test:
 	$(GINKGO) $(GINKGO_ARGS) ./pkg/
+
+test/local/e2e:
+	OPERATOR_NAME=kubernetes-nmstate \
+		$(OPERATOR_SDK) test local ./test/e2e \
+			--kubeconfig $(KUBECONFIG) \
+			--namespace default \
+			--global-manifest deploy/crds/nmstate_v1_nodenetworkstate_crd.yaml \
+			--up-local
+
+test/cluster/e2e:
+	$(OPERATOR_SDK) test local ./test/e2e \
+		--kubeconfig $(KUBECONFIG) \
+		--namespace default \
+		--no-setup
+
+
+$(local_manager_manifest): deploy/operator.yaml
+	sed "s#REPLACE_IMAGE#$(LOCAL_REGISTRY)/$(MANAGER_IMAGE_FULL_NAME)#" \
+		deploy/operator.yaml > $@
+
 
 cluster-up:
 	./cluster/up.sh
@@ -35,11 +73,27 @@ cluster-up:
 cluster-down:
 	./cluster/down.sh
 
-cluster-sync:
-	./cluster/sync.sh
-
 cluster-clean:
 	./cluster/clean.sh
+
+cluster-sync-resources:
+	for resource in $(resources); do \
+		$(KUBECTL) apply -f $$resource; \
+	done
+
+
+cluster-sync-manager: cluster-sync-resources $(local_manager_manifest)
+	IMAGE_REGISTRY=localhost:$(shell ./cluster/cli.sh ports registry | tr -d '\r') \
+		make push-manager
+	./cluster/cli.sh ssh node01 'sudo docker pull $(LOCAL_REGISTRY)/$(MANAGER_IMAGE_FULL_NAME)'
+	# Temporary until image is updated with provisioner that sets this field
+	# This field is required by buildah tool
+	./cluster/cli.sh ssh node01 'sudo sysctl -w user.max_user_namespaces=1024'
+	$(KUBECTL) apply -f deploy/crds/nmstate_v1_nodenetworkstate_crd.yaml
+	$(KUBECTL) delete --ignore-not-found -f $(local_manager_manifest)
+	$(KUBECTL) create -f $(local_manager_manifest)
+
+cluster-sync: cluster-sync-manager
 
 .PHONY: \
 	all \
@@ -49,7 +103,11 @@ cluster-clean:
 	manager \
 	push-manager \
 	test-unit \
+	test/local/e2e \
+	test/cluster/e2e \
 	cluster-up \
 	cluster-down \
+	cluster-sync-resources \
+	cluster-sync-manager \
 	cluster-sync \
 	cluster-clean
---
+++ test/e2e/utils.go
@@ -0,0 +1,105 @@
+package e2e
+
+import (
+	"context"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"testing"
+	"time"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/types"
+	yaml "sigs.k8s.io/yaml"
+
+	framework "github.com/operator-framework/operator-sdk/pkg/test"
+	"github.com/operator-framework/operator-sdk/pkg/test/e2eutil"
+	dynclient "sigs.k8s.io/controller-runtime/pkg/client"
+
+	nmstatev1 "github.com/nmstate/kubernetes-nmstate/pkg/apis/nmstate/v1"
+)
+
+func writePodsLogs(namespace string, writer io.Writer) error {
+	if framework.Global.LocalOperator {
+		return nil
+	}
+	podLogOpts := corev1.PodLogOptions{}
+	podList := &corev1.PodList{}
+	err := framework.Global.Client.List(context.TODO(), &dynclient.ListOptions{}, podList)
+	Expect(err).ToNot(HaveOccurred())
+	podsClientset := framework.Global.KubeClient.CoreV1().Pods(namespace)
+
+	for _, pod := range podList.Items {
+		req := podsClientset.GetLogs(pod.Name, &podLogOpts)
+		podLogs, err := req.Stream()
+		if err != nil {
+			io.WriteString(writer, fmt.Sprintf("error in opening stream: %v\n", err))
+			continue
+		}
+		defer podLogs.Close()
+		_, err = io.Copy(writer, podLogs)
+		if err != nil {
+			io.WriteString(writer, fmt.Sprintf("error in copy information from podLogs to buf: %v\n", err))
+			continue
+		}
+
+	}
+	return nil
+}
+
+func prepare(t *testing.T) (*framework.TestCtx, string) {
+	By("Initialize cluster resources")
+	cleanupRetryInterval := time.Second * 1
+	cleanupTimeout := time.Second * 5
+	ctx := framework.NewTestCtx(t)
+	err := ctx.InitializeClusterResources(&framework.CleanupOptions{TestContext: ctx, Timeout: cleanupTimeout, RetryInterval: cleanupRetryInterval})
+	Expect(err).ToNot(HaveOccurred())
+
+	// get namespace
+	namespace, err := ctx.GetNamespace()
+	Expect(err).ToNot(HaveOccurred())
+
+	err = e2eutil.WaitForOperatorDeployment(t, framework.Global.KubeClient, namespace, "kubernetes-nmstate", 1, time.Second*5, time.Second*30)
+	Expect(err).ToNot(HaveOccurred())
+	return ctx, namespace
+}
+
+func readStateFromFile(file string, namespace string) nmstatev1.NodeNetworkState {
+	manifest, err := ioutil.ReadFile(file)
+	Expect(err).ToNot(HaveOccurred())
+
+	state := nmstatev1.NodeNetworkState{}
+	err = yaml.Unmarshal(manifest, &state)
+	Expect(err).ToNot(HaveOccurred())
+	state.ObjectMeta.Namespace = namespace
+	return state
+}
+
+func createStateFromFile(file string, namespace string, cleanupOptions *framework.CleanupOptions) {
+	state := readStateFromFile(file, namespace)
+	err := framework.Global.Client.Create(context.TODO(), &state, cleanupOptions)
+	Expect(err).ToNot(HaveOccurred())
+}
+
+func updateStateSpecFromFile(file string, key types.NamespacedName) {
+	state := nmstatev1.NodeNetworkState{}
+	stateFromManifest := readStateFromFile(file, key.Namespace)
+	err := framework.Global.Client.Get(context.TODO(), key, &state)
+	Expect(err).ToNot(HaveOccurred())
+	state.Spec = stateFromManifest.Spec
+	err = framework.Global.Client.Update(context.TODO(), &state)
+	Expect(err).ToNot(HaveOccurred())
+}
+
+func nodeNetworkState(key types.NamespacedName) nmstatev1.NodeNetworkState {
+	state := nmstatev1.NodeNetworkState{}
+	timeout := 5 * time.Second
+	interval := 1 * time.Second
+	Eventually(func() error {
+		return framework.Global.Client.Get(context.TODO(), key, &state)
+	}, timeout, interval).ShouldNot(HaveOccurred())
+	return state
+}
