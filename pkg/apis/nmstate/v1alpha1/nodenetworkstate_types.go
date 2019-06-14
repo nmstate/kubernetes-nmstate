62d61c6d439d471b223c8b234e95af1635727a49
HlinaCZ <djhlinacz@gmail.com>
2019-06-14 14:27:22 +0200

    Added condition types
    Added condition types that will be used to report succeses or failures
    of the last configuration.
    We will use this type in following patches.

--- pkg/apis/nmstate/v1alpha1/nodenetworkstate_types.go
+++ pkg/apis/nmstate/v1alpha1/nodenetworkstate_types.go
@@ -26,8 +26,28 @@ type NodeNetworkStateSpec struct {
 // NodeNetworkStateStatus is the status of the NodeNetworkState of a specific node
 // +k8s:openapi-gen=true
 type NodeNetworkStateStatus struct {
-	CurrentState State `json:"currentState,omitempty"`
+	CurrentState State                       `json:"currentState,omitempty"`
+	Condition    []NodeNetworkStateCondition `json:"conditions,omitempty" optional:"true"`
 }
+
+// +k8s:openapi-gen=true
+type NodeNetworkStateCondition struct {
+	Type               NodeNetworkStateConditionType `json:"type"`
+	Reason             string                        `json:"reason,omitempty"`
+	Message            string                        `json:"message,omitempty"`
+	LastHeartbeatTime  metav1.Time                   `json:"lastHearbeatTime,omitempty"`
+	LastTransitionTime metav1.Time                   `json:"lastTransitionTime,omitempty"`
+}
+
+// +k8s:openapi-gen=true
+type NodeNetworkStateConditionType string
+
+const (
+	NodeNetworkStateConditionFailing     NodeNetworkStateConditionType = "Failing"
+	NodeNetworkStateConditionProgressing NodeNetworkStateConditionType = "Progressing"
+	NodeNetworkStateConditionAvailable   NodeNetworkStateConditionType = "Available"
+	NodeNetworkStateConditionInitialized NodeNetworkStateConditionType = "Initialized"
+)
 
 // +genclient
 // +genclient:nonNamespaced
