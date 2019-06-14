5ad0c3f95088d4a1eaea5bba35c125709b2a006e
HlinaCZ <djhlinacz@gmail.com>
2019-06-14 16:08:28 +0200

    ps1 add status

--- pkg/apis/nmstate/v1/nodenetworkstate_types.go
+++ pkg/apis/nmstate/v1/nodenetworkstate_types.go
@@ -1,6 +1,7 @@
 package v1
 
 import (
+	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
@@ -34,6 +35,7 @@ type NodeNetworkStateStatus struct {
 // +k8s:openapi-gen=true
 type NodeNetworkStateCondition struct {
 	Type               NodeNetworkStateConditionType `json:"type"`
+	Status             corev1.ConditionStatus        `json:"status"`
 	Reason             string                        `json:"reason,omitempty"`
 	Message            string                        `json:"message,omitempty"`
 	LastHeartbeatTime  metav1.Time                   `json:"lastHearbeatTime,omitempty"`
