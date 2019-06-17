738a3e03cf05466ffdd6025f5c988979b2f9f9b7
Quique Llorente <ellorent@redhat.com>
2019-06-17 15:01:41 +0200

    [90] Add refresh at node controller
    This will reconcile Node every 5 seconds re recreate in case of
    missing nodenetworkstate
    
    Signed-off-by: Quique Llorente <ellorent@redhat.com>

--- test/e2e/nodes_test.go
+++ test/e2e/nodes_test.go
@@ -14,12 +14,9 @@ import (
 )
 
 var _ = Describe("Nodes", func() {
-	Context("when nodes are up", func() {
-		AfterEach(func() {
-			deleteNodeNeworkStates()
-		})
+	Context("when are up", func() {
 		var (
-			timeout  = 5 * time.Second
+			timeout  = 10 * time.Second
 			interval = 1 * time.Second
 		)
 		It("should have NodeNetworkState with currentState for each node", func() {
@@ -44,6 +41,21 @@ var _ = Describe("Nodes", func() {
 					ContainElement("eth0"),
 				))
 			}
+		})
+		Context("and node network state is deleted", func() {
+			BeforeEach(func() {
+				deleteNodeNeworkStates()
+			})
+			It("should recreate it with currentState", func() {
+				for _, node := range nodes {
+					key := types.NamespacedName{Namespace: namespace, Name: node}
+					var currentStateYaml nmstatev1.State
+					Eventually(func() nmstatev1.State {
+						currentStateYaml = nodeNetworkState(key).Status.CurrentState
+						return currentStateYaml
+					}, timeout, interval).ShouldNot(BeEmpty(), "Node %s should have currentState", node)
+				}
+			})
 		})
 	})
 })
