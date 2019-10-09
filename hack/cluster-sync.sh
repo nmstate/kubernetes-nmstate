bdb85ee1a628d540d08b8903dae3e1a41869ddbe
HlinaCZ <djhlinacz@gmail.com>
2019-10-09 14:53:20 +0200

    ps1

--- hack/cluster-sync.sh
+++ hack/cluster-sync.sh
@@ -4,7 +4,7 @@ ${KUBECTL} apply -f deploy/crds/nmstate_v1alpha1_nodenetworkstate_crd.yaml
 ${KUBECTL} apply -f deploy/crds/nmstate_v1alpha1_nodenetworkconfigurationpolicy_crd.yaml
 ${KUBECTL} delete --ignore-not-found -f ${local_handler_manifest}
 # Set debug verbosity level for logs when using cluster-sync
-sed "s#--v=production#--v=debug#" $(local_handler_manifest) | $(KUBECTL) create -f -
+sed "s#--v=production#--v=debug#" ${local_handler_manifest} | ${KUBECTL} create -f -
 
 desiredNumberScheduled="$(${KUBECTL} get daemonset -n nmstate nmstate-handler -o=jsonpath='{.status.desiredNumberScheduled}')"
 for i in {300..0}; do
