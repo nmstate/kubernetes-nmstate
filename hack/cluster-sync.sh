4187b6f19020b100e99b3e367b6ab79d29341ac6
Félix Enrique Llorente Pastora <ellorent@redhat.com>
2019-10-10 13:12:38 +0200

    Retry desiredNumberScheduler, it takes time to be filled in (#213)
    If we just do it ones at the beginning sometimes is '0' so cluster sync never happends
    
    Signed-off-by: Quique Llorente <ellorent@redhat.com>

--- hack/cluster-sync.sh
+++ hack/cluster-sync.sh
@@ -6,17 +6,21 @@ ${KUBECTL} delete --ignore-not-found -f ${local_handler_manifest}
 # Set debug verbosity level for logs when using cluster-sync
 sed "s#--v=production#--v=debug#" ${local_handler_manifest} | ${KUBECTL} create -f -
 
-desiredNumberScheduled="$(${KUBECTL} get daemonset -n nmstate nmstate-handler -o=jsonpath='{.status.desiredNumberScheduled}')"
 for i in {300..0}; do
-	if [ $desiredNumberScheduled == "$(${KUBECTL} get daemonset -n nmstate nmstate-handler -o=jsonpath='{.status.numberAvailable}')" ]; then
-		echo "nmstate-handler DS is ready"
-		break
-	fi
+    # We have to re-check desired number, sometimes takes some time to be filled in
+    desiredNumberScheduled=$(${KUBECTL} get daemonset -n nmstate nmstate-handler -o=jsonpath='{.status.desiredNumberScheduled}')
 
-	if [ $i -eq 0 ]; then
-		echo "nmstate-handler DS haven't turned ready within the given timeout"
-		exit 1
-	fi
+    numberAvailable=$(${KUBECTL} get daemonset -n nmstate nmstate-handler -o=jsonpath='{.status.numberAvailable}')
 
-	sleep 1
+    if [ "$desiredNumberScheduled" == "$numberAvailable" ]; then
+        echo "nmstate-handler DS is ready"
+        break
+    fi
+
+    if [ $i -eq 0 ]; then
+        echo "nmstate-handler DS haven't turned ready within the given timeout"
+    exit 1
+    fi
+
+    sleep 1
 done
