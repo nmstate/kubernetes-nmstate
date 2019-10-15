d99400089f668194ac41c2ea58668aa4187aa2c1
Federico Paolinelli <fpaoline@redhat.com>
2019-10-15 18:04:54 +0200

    Remove unnecessary context
    Signed-off-by: Federico Paolinelli <fpaoline@redhat.com>

--- test/e2e/simple_ovs_bridge.go
+++ test/e2e/simple_ovs_bridge.go
@@ -6,55 +6,53 @@ import (
 )
 
 var _ = Describe("Simple OVS bridge", func() {
-	Context("when desiredState is configured", func() {
-		Context("with an ovs bridge up", func() {
-			BeforeEach(func() {
-				updateDesiredState(ovsBrUp(bridge1))
-			})
-			AfterEach(func() {
-				updateDesiredState(ovsBrAbsent(bridge1))
-				for _, node := range nodes {
-					interfacesNameForNodeEventually(node).ShouldNot(ContainElement(bridge1))
-				}
-				resetDesiredStateForNodes()
-			})
-			It("should have the ovs bridge at currentState", func() {
-				for _, node := range nodes {
-					interfacesForNode(node).Should(ContainElement(SatisfyAll(
+	Context("when desiredState is configured with an ovs bridge up", func() {
+		BeforeEach(func() {
+			updateDesiredState(ovsBrUp(bridge1))
+		})
+		AfterEach(func() {
+			updateDesiredState(ovsBrAbsent(bridge1))
+			for _, node := range nodes {
+				interfacesNameForNodeEventually(node).ShouldNot(ContainElement(bridge1))
+			}
+			resetDesiredStateForNodes()
+		})
+		It("should have the ovs bridge at currentState", func() {
+			for _, node := range nodes {
+				interfacesForNode(node).Should(ContainElement(SatisfyAll(
+					HaveKeyWithValue("name", bridge1),
+					HaveKeyWithValue("type", "ovs-bridge"),
+					HaveKeyWithValue("state", "up"),
+				)))
+			}
+		})
+	})
+	Context("when desiredState is configured with an ovs bridge with internal port up", func() {
+		BeforeEach(func() {
+			updateDesiredState(ovsbBrWithInternalInterface(bridge1))
+		})
+		AfterEach(func() {
+			updateDesiredState(ovsBrAbsent(bridge1))
+			for _, node := range nodes {
+				interfacesNameForNodeEventually(node).ShouldNot(ContainElement(bridge1))
+			}
+			resetDesiredStateForNodes()
+		})
+		It("should have the ovs bridge at currentState", func() {
+			for _, node := range nodes {
+				interfacesForNode(node).Should(SatisfyAll(
+					ContainElement(SatisfyAll(
 						HaveKeyWithValue("name", bridge1),
 						HaveKeyWithValue("type", "ovs-bridge"),
 						HaveKeyWithValue("state", "up"),
-					)))
-				}
-			})
-		})
-		Context("with an ovs bridge with internal port up", func() {
-			BeforeEach(func() {
-				updateDesiredState(ovsbBrWithInternalInterface(bridge1))
-			})
-			AfterEach(func() {
-				updateDesiredState(ovsBrAbsent(bridge1))
-				for _, node := range nodes {
-					interfacesNameForNodeEventually(node).ShouldNot(ContainElement(bridge1))
-				}
-				resetDesiredStateForNodes()
-			})
-			It("should have the ovs bridge at currentState", func() {
-				for _, node := range nodes {
-					interfacesForNode(node).Should(SatisfyAll(
-						ContainElement(SatisfyAll(
-							HaveKeyWithValue("name", bridge1),
-							HaveKeyWithValue("type", "ovs-bridge"),
-							HaveKeyWithValue("state", "up"),
-						)),
-						ContainElement(SatisfyAll(
-							HaveKeyWithValue("name", "ovs0"),
-							HaveKeyWithValue("type", "ovs-interface"),
-							HaveKeyWithValue("state", "up"),
-						)),
-					))
-				}
-			})
+					)),
+					ContainElement(SatisfyAll(
+						HaveKeyWithValue("name", "ovs0"),
+						HaveKeyWithValue("type", "ovs-interface"),
+						HaveKeyWithValue("state", "up"),
+					)),
+				))
+			}
 		})
 	})
 })
