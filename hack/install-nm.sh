b60b014c896026d34155e03d6b6d4ed08d2ef444
Federico Paolinelli <fpaoline@redhat.com>
2019-10-15 16:47:32 +0200

    Use SSH and KUBECTL exported by the Makefile
    Signed-off-by: Federico Paolinelli <fpaoline@redhat.com>

--- hack/install-nm.sh
+++ hack/install-nm.sh
@@ -1,18 +1,15 @@
 #!/bin/bash -e
 
-ssh=kubevirtci/cluster-up/ssh.sh
-kubectl=kubevirtci/cluster-up/kubectl.sh
-
 function install_nm_on_node() {
     node=$1
-    $ssh $node sudo -- yum install -y NetworkManager NetworkManager-ovs
-    $ssh $node sudo -- systemctl daemon-reload
-    $ssh $node sudo -- systemctl restart NetworkManager
+    $SSH $node sudo -- yum install -y NetworkManager NetworkManager-ovs
+    $SSH $node sudo -- systemctl daemon-reload
+    $SSH $node sudo -- systemctl restart NetworkManager
     echo "Check NetworkManager is working fine on node $node"
-    $ssh $node -- nmcli device show > /dev/null
+    $SSH $node -- nmcli device show > /dev/null
 }
 
 echo 'Install NetworkManager on nodes'
-for node in $($kubectl get nodes --no-headers | awk '{print $1}'); do
+for node in $($KUBECTL get nodes --no-headers | awk '{print $1}'); do
     install_nm_on_node "$node"
 done
--- hack/install-ovs.sh
+++ hack/install-ovs.sh
@@ -1,21 +1,18 @@
 #!/bin/bash -e
 
-ssh=kubevirtci/cluster-up/ssh.sh
-kubectl=kubevirtci/cluster-up/kubectl.sh
-
 
 function install_ovs_on_node() {
     node=$1
-    $ssh $node -- sudo yum install -y http://cbs.centos.org/kojifiles/packages/openvswitch/2.9.2/1.el7/x86_64/openvswitch-2.9.2-1.el7.x86_64.rpm http://cbs.centos.org/kojifiles/packages/openvswitch/2.9.2/1.el7/x86_64/openvswitch-devel-2.9.2-1.el7.x86_64.rpm http://cbs.centos.org/kojifiles/packages/dpdk/17.11/3.el7/x86_64/dpdk-17.11-3.el7.x86_64.rpm
-    $ssh $node -- sudo systemctl daemon-reload
-    $ssh $node -- sudo systemctl restart openvswitch
+    $SSH $node -- sudo yum install -y http://cbs.centos.org/kojifiles/packages/openvswitch/2.9.2/1.el7/x86_64/openvswitch-2.9.2-1.el7.x86_64.rpm http://cbs.centos.org/kojifiles/packages/openvswitch/2.9.2/1.el7/x86_64/openvswitch-devel-2.9.2-1.el7.x86_64.rpm http://cbs.centos.org/kojifiles/packages/dpdk/17.11/3.el7/x86_64/dpdk-17.11-3.el7.x86_64.rpm
+    $SSH $node -- sudo systemctl daemon-reload
+    $SSH $node -- sudo systemctl restart openvswitch
 }
 
 # we currently skip ovs for non k8s providers.
 if [[ "$KUBEVIRT_PROVIDER" =~  k8s- ]]; then
     echo 'Installing Open vSwitch on nodes'
 
-    for node in $($kubectl get nodes --no-headers | awk '{print $1}'); do
+    for node in $($KUBECTL get nodes --no-headers | awk '{print $1}'); do
         install_ovs_on_node "$node"
     done
 fi
