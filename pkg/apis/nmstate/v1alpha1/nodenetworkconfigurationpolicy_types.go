4b5b8d5fdc2e702c022b2d7df393159859c434b5
Petr Horacek <phoracek@redhat.com>
2019-11-12 17:44:53 +0100

    add list of conditions to policy (#247)
    * add list of conditions to policy
    
    This list will be used to keep configuration status per each node
    in Policy's State.
    
    That will be a temporary solution until we introduce Enactment object.
    
    The benefit of this is, that we keep the status of configuration in
    the object that is used to trigger the configuration. In other words,
    we decouple reporting and configuration.
    
    * add policy condition list helpers
    
    These helpers will be used to A) set configuration condition for a specific node
    and B) to obtain condition of certain type for a given node.
    
    Signed-off-by: Petr Horacek <phoracek@redhat.com>

--- pkg/apis/nmstate/v1alpha1/nodenetworkconfigurationpolicy_types.go
+++ pkg/apis/nmstate/v1alpha1/nodenetworkconfigurationpolicy_types.go
@@ -1,38 +1,19 @@
 package v1alpha1
 
 import (
+	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 // NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
 
-// NodeNetworkConfigurationPolicySpec defines the desired state of NodeNetworkConfigurationPolicy
-// +k8s:openapi-gen=true
-type NodeNetworkConfigurationPolicySpec struct {
-	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
-	// Important: Run "operator-sdk generate k8s" to regenerate code after modifying this file
-	// Add custom validation using kubebuilder tags: https://book.kubebuilder.io/beyond_basics/generating_crd.html
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
 
-	// In case of multiple policies applying for the same node this
-	// priority define the order from low to high
-	Priority int `json:"priority,omitempty"`
-
-	// NodeSelector is a selector which must be true for the policy to be applied to the node.
-	// Selector which must match a node's labels for the policy to be scheduled on that node.
-	// More info: https://kubernetes.io/docs/concepts/configuration/assign-pod-node/
-	// +optional
-	NodeSelector map[string]string `json:"nodeSelector,omitempty"`
-
-	// The desired configuration of the policy
-	DesiredState State `json:"desiredState,omitempty"`
-}
-
-// NodeNetworkConfigurationPolicyStatus defines the observed state of NodeNetworkConfigurationPolicy
-// +k8s:openapi-gen=true
-type NodeNetworkConfigurationPolicyStatus struct {
-	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
-	// Important: Run "operator-sdk generate k8s" to regenerate code after modifying this file
-	// Add custom validation using kubebuilder tags: https://book.kubebuilder.io/beyond_basics/generating_crd.html
+// NodeNetworkConfigurationPolicyList contains a list of NodeNetworkConfigurationPolicy
+type NodeNetworkConfigurationPolicyList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []NodeNetworkConfigurationPolicy `json:"items"`
 }
 
 // +genclient
@@ -51,14 +32,80 @@ type NodeNetworkConfigurationPolicy struct {
 	Status NodeNetworkConfigurationPolicyStatus `json:"status,omitempty"`
 }
 
-// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+// NodeNetworkConfigurationPolicySpec defines the desired state of NodeNetworkConfigurationPolicy
+// +k8s:openapi-gen=true
+type NodeNetworkConfigurationPolicySpec struct {
+	// NodeSelector is a selector which must be true for the policy to be applied to the node.
+	// Selector which must match a node's labels for the policy to be scheduled on that node.
+	// More info: https://kubernetes.io/docs/concepts/configuration/assign-pod-node/
+	// +optional
+	NodeSelector map[string]string `json:"nodeSelector,omitempty"`
 
-// NodeNetworkConfigurationPolicyList contains a list of NodeNetworkConfigurationPolicy
-type NodeNetworkConfigurationPolicyList struct {
-	metav1.TypeMeta `json:",inline"`
-	metav1.ListMeta `json:"metadata,omitempty"`
-	Items           []NodeNetworkConfigurationPolicy `json:"items"`
+	// The desired configuration of the policy
+	DesiredState State `json:"desiredState,omitempty"`
 }
+
+// NodeNetworkConfigurationPolicyStatus defines the observed state of NodeNetworkConfigurationPolicy
+// +k8s:openapi-gen=true
+type NodeNetworkConfigurationPolicyStatus struct {
+	Enactments EnactmentList `json:"enactments,omitempty" optional:"true"`
+}
+
+// TODO: This is a temporary solution. This list will be replaced by a dedicated
+// NodeNetworkConfigurationEnactment object.
+type EnactmentList []Enactment
+
+type Enactment struct {
+	NodeName   string        `json:"nodeName"`
+	Conditions ConditionList `json:"conditions,omitempty"`
+}
+
+func NewEnactment(nodeName string) Enactment {
+	return Enactment{
+		NodeName:   nodeName,
+		Conditions: ConditionList{},
+	}
+}
+
+func (enactments *EnactmentList) SetCondition(nodeName string, conditionType ConditionType, status corev1.ConditionStatus, reason ConditionReason, message string) {
+	enactment := enactments.find(nodeName)
+
+	if enactment == nil {
+		enactment := NewEnactment(nodeName)
+		enactment.Conditions.Set(conditionType, status, reason, message)
+		*enactments = append(*enactments, enactment)
+		return
+	}
+
+	enactment.Conditions.Set(conditionType, status, reason, message)
+}
+
+func (enactments EnactmentList) FindCondition(nodeName string, conditionType ConditionType) *Condition {
+	enactment := enactments.find(nodeName)
+	if enactment == nil {
+		return nil
+	}
+	return enactment.Conditions.Find(conditionType)
+}
+
+func (enactments EnactmentList) find(nodeName string) *Enactment {
+	for i, enactment := range enactments {
+		if enactment.NodeName == nodeName {
+			return &enactments[i]
+		}
+	}
+	return nil
+}
+
+const (
+	NodeNetworkConfigurationPolicyConditionAvailable ConditionType = "Available"
+	NodeNetworkConfigurationPolicyConditionFailing   ConditionType = "Failing"
+)
+
+const (
+	NodeNetworkConfigurationPolicyConditionFailedToConfigure      ConditionReason = "FailedToConfigure"
+	NodeNetworkConfigurationPolicyConditionSuccessfullyConfigured ConditionReason = "SuccessfullyConfigured"
+)
 
 func init() {
 	SchemeBuilder.Register(&NodeNetworkConfigurationPolicy{}, &NodeNetworkConfigurationPolicyList{})
