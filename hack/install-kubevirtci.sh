cbda326203a059c31445b1b71ee5541dbf62aa3c
Petr Horacek <phoracek@redhat.com>
2019-12-08 15:18:48 +0100

    bump kubevirtci version
    Signed-off-by: Petr Horacek <phoracek@redhat.com>

--- hack/install-kubevirtci.sh
+++ hack/install-kubevirtci.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 
 organization=kubevirt
-commit="6132f2efcf2aa2d48f256a409fe90cc0b74c3563"
+commit="d623d399e81c7baf4af84bb903341606e887c83b"
 
 script_dir=$(dirname "$(readlink -f "$0")")
 kubevirtci_dir=kubevirtci
