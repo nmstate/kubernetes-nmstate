31193da6b412217a35b0657b2fdc9d07dcf41d2b
Félix Enrique Llorente Pastora <ellorent@redhat.com>
2020-01-13 10:02:16 +0100

    Add script to automatize github release (#330)
    It will instruct github to use GITHUB_USER and GITHUB_TOKEN as
    user password and call `make release`
    
    Signed-off-by: Quique Llorente <ellorent@redhat.com>

---
+++ automation/release.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -xe
+
+# This script tags current version and create a new release at github and pushes
+# to quay.io
+# To run it just do proper docker login and pass the git hub user password/token as
+# GITHUB_USER and GITHUB_TOKEN env variables.
+
+# To pass user/password from automations, idea is taken from [1]
+# [1] https://stackoverflow.com/questions/8536732/can-i-hold-git-credentials-in-environment-variables
+git config credential.helper '!f() { sleep 1; echo "username=${GITHUB_USER}"; echo "password=${GITHUB_TOKEN}"; }; f'
+
+source automation/check-patch.setup.sh
+cd ${TMP_PROJECT_PATH}
+make \
+    IMAGE_REGISTRY=${IMAGE_REGISTRY:-quay.io}  \
+    IMAGE_REPO=${IMAGE_REPO:-nmstate} \
+    release
