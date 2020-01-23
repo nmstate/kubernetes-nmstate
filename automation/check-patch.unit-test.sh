0faf8f3b6adf4a2ee6d97177c35050d28d5713f5
Quique Llorente <ellorent@redhat.com>
2020-01-23 14:35:36 +0100

    Always copy junit reports at CI
    Signed-off-by: Quique Llorente <ellorent@redhat.com>

--- automation/check-patch.unit-test.sh
+++ automation/check-patch.unit-test.sh
@@ -7,12 +7,16 @@
 # yum -y install automation/check-patch.packages
 # automation/check-patch.e2e-k8s.sh
 
+teardown() {
+    cp $(find . -name "*junit*.xml") $ARTIFACTS
+}
+
 main() {
     source automation/check-patch.setup.sh
     cd ${TMP_PROJECT_PATH}
     make all
+    trap teardown EXIT SIGINT SIGTERM SIGSTOP
     make UNIT_TEST_ARGS="-noColor --compilers=2" test/unit
-    cp $(find . -name "*junit*.xml") $ARTIFACTS
 }
 
 [[ "${BASH_SOURCE[0]}" == "$0" ]] && main "$@"
