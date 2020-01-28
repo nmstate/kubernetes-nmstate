f6b75caab757eef1c0162a345d6d1d93d36a4380
Félix Enrique Llorente Pastora <ellorent@redhat.com>
2020-01-28 11:31:53 +0100

    Pin and install go from go.mod version (#352)
    This include a super low tech mechanism not docker multistage either
    dockerized golang, it just download a golang tarball and install it under
    repo.
    
    Signed-off-by: Quique Llorente <ellorent@redhat.com>

--- automation/check-patch.setup.sh
+++ automation/check-patch.setup.sh
@@ -6,22 +6,11 @@
 tmp_dir=/tmp/knmstate/
 
 rm -rf $tmp_dir
-
-echo 'Setup Go paths'
-export GOROOT=$tmp_dir/go/root
-mkdir -p $GOROOT
-export PATH=${GOROOT}/bin:${PATH}
-
-export GIMME_GO_VERSION=$(grep "^go " go.mod |awk '{print $2}')
-echo "Install Go $GIMME_GO_VERSION"
-gimme_dir=$tmp_dir/go/gimme
-mkdir -p $gimme_dir
-curl -sL https://raw.githubusercontent.com/travis-ci/gimme/master/gimme | HOME=${gimme_dir} bash >> ${gimme_dir}/gimme.sh
-source $gimme_dir/gimme.sh
-
+mkdir -p $tmp_dir
 
 export TMP_PROJECT_PATH=$tmp_dir/kubernetes-nmstate
 export ARTIFACTS=${ARTIFACTS-$tmp_dir/artifacts}
 mkdir -p $ARTIFACTS
+
 rsync -rt --links --filter=':- .gitignore' $(pwd)/ $TMP_PROJECT_PATH
 
