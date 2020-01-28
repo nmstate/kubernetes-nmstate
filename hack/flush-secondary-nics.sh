8481936b71fecd4b589a0ebc605b21a25b31719f
Quique Llorente <ellorent@redhat.com>
2020-01-28 16:12:04 +0100

    review fixes
    Signed-off-by: Quique Llorente <ellorent@redhat.com>

--- hack/flush-secondary-nics.sh
+++ hack/flush-secondary-nics.sh
@@ -1,10 +1,17 @@
 #!/bin/bash -e
+#
+#    Flush ip address at secondary nics
+#
+#    Our e2e tests expect secondary nics without IP addresses also
+#    the ip addresses assigned by kubevirtci dnsmasq has the same CIDR
+#    as the primary nic, this make impossible to test default-bridge
+#    test propertly.
+#
 
-# TODO: Iterate all the nodes
 script_dir=$(dirname "$(readlink -f "$0")")
 ssh=$script_dir/../kubevirtci/cluster-up/ssh.sh
 
-# TODO okd/ocp
+# TODO: flush IPs at okd/ocp provider too.
 if [[ "$KUBEVIRT_PROVIDER" =~ k8s ]]; then
     for node in $($KUBECTL get nodes --no-headers | awk '{print $1}'); do
         for n in $(seq 1 $KUBEVIRT_NUM_SECONDARY_NICS); do
