04751db7eb61efbe1a9466cc908b4f9436b0832f
Petr Horacek <phoracek@redhat.com>
2020-02-11 13:43:26 +0100

    docs: 103 troubleshooting (#395)
    Signed-off-by: Petr Horacek <phoracek@redhat.com>

--- test/e2e/demos_test.go
+++ test/e2e/demos_test.go
@@ -23,7 +23,28 @@ func skipIfNotKubernetes() {
 	}
 }
 
-var _ = Describe("Introduction: Configuration", func() {
+var _ = Describe("Introduction", func() {
+	runConfiguration := func() {
+		kubectlAndCheck("apply", "-f", "docs/examples/bond0-eth1-eth2_up.yaml")
+		kubectlAndCheck("wait", "nncp", "bond0-eth1-eth2", "--for", "condition=Available", "--timeout", "2m")
+		kubectlAndCheck("apply", "-f", "docs/examples/bond0-eth1-eth2_absent.yaml")
+		kubectlAndCheck("wait", "nncp", "bond0-eth1-eth2", "--for", "condition=Available", "--timeout", "2m")
+		kubectlAndCheck("delete", "nncp", "bond0-eth1-eth2")
+
+		kubectlAndCheck("apply", "-f", "docs/examples/eth1-eth2_up.yaml")
+		kubectlAndCheck("wait", "nncp", "eth1", "eth2", "--for", "condition=Available", "--timeout", "2m")
+		kubectlAndCheck("delete", "nncp", "eth1", "eth2")
+
+		kubectlAndCheck("apply", "-f", "docs/examples/vlan100_node01_up.yaml")
+		kubectlAndCheck("wait", "nncp", "vlan100", "--for", "condition=Available", "--timeout", "2m")
+	}
+
+	runTroubleshooting := func() {
+		kubectlAndCheck("apply", "-f", "docs/examples/eth666_up.yaml")
+		kubectlAndCheck("wait", "nncp", "eth666", "--for", "condition=Degraded", "--timeout", "2m")
+		kubectlAndCheck("delete", "nncp", "eth666")
+	}
+
 	BeforeEach(func() {
 		skipIfNotKubernetes()
 	})
@@ -34,18 +55,22 @@ var _ = Describe("Introduction: Configuration", func() {
 		resetDesiredStateForNodes()
 	})
 
-	Context("following the tutorial", func() {
+	Context("Configuration tutorial", func() {
 		It("should succeed executing all the commands", func() {
-			kubectlAndCheck("apply", "-f", "docs/examples/bond0-eth1-eth2_up.yaml")
-			kubectlAndCheck("wait", "nncp", "bond0-eth1-eth2", "--for", "condition=Available", "--timeout", "2m")
-			kubectlAndCheck("apply", "-f", "docs/examples/bond0-eth1-eth2_absent.yaml")
-			kubectlAndCheck("wait", "nncp", "bond0-eth1-eth2", "--for", "condition=Available", "--timeout", "2m")
-			kubectlAndCheck("delete", "nncp", "bond0-eth1-eth2")
-			kubectlAndCheck("apply", "-f", "docs/examples/eth1-eth2_up.yaml")
-			kubectlAndCheck("wait", "nncp", "eth1", "eth2", "--for", "condition=Available", "--timeout", "2m")
-			kubectlAndCheck("delete", "nncp", "eth1", "eth2")
-			kubectlAndCheck("apply", "-f", "docs/examples/vlan100_node01_up.yaml")
-			kubectlAndCheck("wait", "nncp", "vlan100", "--for", "condition=Available", "--timeout", "2m")
+			runConfiguration()
+		})
+	})
+
+	Context("Troubleshooting tutorial", func() {
+		It("should succeed executing all the commands", func() {
+			runTroubleshooting()
+		})
+	})
+
+	Context("All tutorials in a row", func() {
+		It("should succeed executing all the commands", func() {
+			runConfiguration()
+			runTroubleshooting()
 		})
 	})
 })
