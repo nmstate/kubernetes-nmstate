4534f6a565399d08f5550732c518ba4b4da10cda
Petr Horacek <phoracek@redhat.com>
2020-02-12 18:29:27 +0100

    Revert "use NM 1.22 on Kubernetes kubevirtci" (#410)
    This reverts commit 77bf8e7e6c156ed8047129cecc2893a3a4d055d4.
    
    We will branch out for 1.20 branch. Let's make sure we test the correct
    version. Once we branch out, we can revert this.
    
    Signed-off-by: Petr Horacek <phoracek@redhat.com>

--- hack/install-nm.sh
+++ hack/install-nm.sh
@@ -4,7 +4,7 @@ function install_nm_on_node() {
     node=$1
     # Use copr repository to get newer NetworkManager
     $SSH $node sudo -- yum install -y yum-plugin-copr
-    $SSH $node sudo -- yum copr enable -y networkmanager/NetworkManager-1.22-git
+    $SSH $node sudo -- yum copr enable -y networkmanager/NetworkManager-1.20
     $SSH $node sudo -- yum install -y NetworkManager NetworkManager-ovs
     $SSH $node sudo -- systemctl daemon-reload
     $SSH $node sudo -- systemctl restart NetworkManager
