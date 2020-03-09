a906d929f292d22d9888478ff79b15350bcba7d9
Félix Enrique Llorente Pastora <ellorent@redhat.com>
2020-03-09 16:43:13 +0100

    Add consistently check at cluster-sync (#448)
    To check state switching after deploy, this checks that everything is good
    for some time, after deploy it.
    
    Signed-off-by: Quique Llorente <ellorent@redhat.com>

--- cluster/clean.sh
+++ cluster/clean.sh
@@ -4,11 +4,14 @@ set -ex
 
 echo 'Cleaning up ...'
 
-./cluster/kubectl.sh delete --ignore-not-found -f deploy/
-./cluster/kubectl.sh delete --ignore-not-found -f deploy/crds/nmstate.io_nodenetworkconfigurationenactments_crd.yaml
-./cluster/kubectl.sh delete --ignore-not-found -f deploy/crds/nmstate.io_nodenetworkconfigurationpolicies_crd.yaml
-./cluster/kubectl.sh delete --ignore-not-found -f deploy/crds/nmstate.io_nodenetworkstates_crd.yaml
+kubectl=./cluster/kubectl.sh
+manifests_dir=deploy
+
+$kubectl delete --ignore-not-found -f $manifests_dir/
+$kubectl delete --ignore-not-found -f $manifests_dir/crds/nmstate.io_nodenetworkconfigurationenactments_crd.yaml
+$kubectl delete --ignore-not-found -f $manifests_dir/crds/nmstate.io_nodenetworkconfigurationpolicies_crd.yaml
+$kubectl delete --ignore-not-found -f $manifests_dir/crds/nmstate.io_nodenetworkstates_crd.yaml
 
 if [[ "$KUBEVIRT_PROVIDER" =~ ^(okd|ocp)-.*$ ]]; then
-    ./cluster/kubectl.sh delete --ignore-not-found -f deploy/openshift/
+    $kubectl delete --ignore-not-found -f $manifests_dir/openshift/
 fi
--- cluster/sync.sh
+++ cluster/sync.sh
@@ -2,80 +2,101 @@
 
 set -ex
 
+kubectl=./cluster/kubectl.sh
+manifests_dir=deploy
+
 function getDesiredNumberScheduled {
-    echo $(./cluster/kubectl.sh get daemonset -n nmstate $1 -o=jsonpath='{.status.desiredNumberScheduled}')
+    $kubectl get daemonset -n nmstate $1 -o=jsonpath='{.status.desiredNumberScheduled}'
 }
 
 function getNumberAvailable {
-    numberAvailable=$(./cluster/kubectl.sh get daemonset -n nmstate $1 -o=jsonpath='{.status.numberAvailable}')
+    numberAvailable=$($kubectl get daemonset -n nmstate $1 -o=jsonpath='{.status.numberAvailable}')
     echo ${numberAvailable:-0}
 }
 
-function consistently {
+function eventually {
+    timeout=30
+    interval=5
     cmd=$@
-    retries=3
-    interval=1
-    cnt=1
-    while [[ $cnt -le $retries ]]; do
-        $cmd
+    echo "Checking eventually $cmd"
+    while ! $cmd; do
         sleep $interval
-        cnt=$(($cnt + 1))
+        timeout=$(( $timeout - $interval ))
+        if [ $timeout -le 0 ]; then
+            return 1
+        fi
+    done
+}
+
+function consistently {
+    timeout=10
+    interval=1
+    cmd=$@
+    echo "Checking consistently $cmd"
+    while $cmd; do
+        sleep $interval
+        timeout=$(( $timeout - $interval ))
+        if [ $timeout -le 0 ]; then
+            return 0
+        fi
     done
 }
 
 function isOk {
     desiredNumberScheduled=$(getDesiredNumberScheduled $1)
     numberAvailable=$(getNumberAvailable $1)
-
-    if [ "$desiredNumberScheduled" == "$numberAvailable" ]; then
-        echo "$1 DS is ready"
-        return 0
-    else
-        return 1
-    fi
+    [ "$desiredNumberScheduled" == "$numberAvailable" ]
 }
 
-# Cleanup previous deployment, if there is any
-make cluster-clean
+function deploy() {
+    # Cleanup previous deployment, if there is any
+    make cluster-clean
 
-# Fetch registry port that can be used to upload images to the local kubevirtci cluster
-registry_port=$(./cluster/cli.sh ports registry | tr -d '\r')
-if [[ "${KUBEVIRT_PROVIDER}" =~ ^(okd|ocp)-.*$ ]]; then \
-		registry=localhost:$(./cluster/cli.sh ports --container-name=cluster registry | tr -d '\r')
-else
-    registry=localhost:$(./cluster/cli.sh ports registry | tr -d '\r')
-fi
-
-# Build new handler image from local sources and push it to the kubevirtci cluster
-IMAGE_REGISTRY=${registry} make push-handler
-
-# Deploy all needed manifests
-./cluster/kubectl.sh apply -f deploy/namespace.yaml
-./cluster/kubectl.sh apply -f deploy/service_account.yaml
-./cluster/kubectl.sh apply -f deploy/role.yaml
-./cluster/kubectl.sh apply -f deploy/role_binding.yaml
-./cluster/kubectl.sh apply -f deploy/crds/nmstate.io_nodenetworkstates_crd.yaml
-./cluster/kubectl.sh apply -f deploy/crds/nmstate.io_nodenetworkconfigurationpolicies_crd.yaml
-./cluster/kubectl.sh apply -f deploy/crds/nmstate.io_nodenetworkconfigurationenactments_crd.yaml
-if [[ "$KUBEVIRT_PROVIDER" =~ ^(okd|ocp)-.*$ ]]; then
-		./cluster/kubectl.sh apply -f deploy/openshift/
-fi
-sed \
-    -e "s#--v=production#--v=debug#" \
-    -e "s#REPLACE_IMAGE#registry:5000/nmstate/kubernetes-nmstate-handler#" \
-    deploy/operator.yaml | ./cluster/kubectl.sh create -f -
-
-# Wait until the handler becomes ready on all nodes
-for i in {300..0}; do
-    # We have to re-check desired number, sometimes takes some time to be filled in
-    if consistently isOk nmstate-handler && consistently isOk nmstate-handler-worker; then
-       break
+    # Fetch registry port that can be used to upload images to the local kubevirtci cluster
+    registry_port=$(./cluster/cli.sh ports registry | tr -d '\r')
+    if [[ "${KUBEVIRT_PROVIDER}" =~ ^(okd|ocp)-.*$ ]]; then \
+            registry=localhost:$(./cluster/cli.sh ports --container-name=cluster registry | tr -d '\r')
+    else
+        registry=localhost:$(./cluster/cli.sh ports registry | tr -d '\r')
     fi
 
-    if [ $i -eq 0 ]; then
-        echo "nmstate-handler or nmstate-handler-worker DS haven't turned ready within the given timeout"
-    exit 1
-    fi
+    # Build new handler image from local sources and push it to the kubevirtci cluster
+    IMAGE_REGISTRY=${registry} make push-handler
 
-    sleep 1
-done
+    # Deploy all needed manifests
+    $kubectl apply -f $manifests_dir/namespace.yaml
+    $kubectl apply -f $manifests_dir/service_account.yaml
+    $kubectl apply -f $manifests_dir/role.yaml
+    $kubectl apply -f $manifests_dir/role_binding.yaml
+    $kubectl apply -f $manifests_dir/crds/nmstate.io_nodenetworkstates_crd.yaml
+    $kubectl apply -f $manifests_dir/crds/nmstate.io_nodenetworkconfigurationpolicies_crd.yaml
+    $kubectl apply -f $manifests_dir/crds/nmstate.io_nodenetworkconfigurationenactments_crd.yaml
+    if [[ "$KUBEVIRT_PROVIDER" =~ ^(okd|ocp)-.*$ ]]; then
+            $kubectl apply -f $manifests_dir/openshift/
+    fi
+    sed \
+        -e "s#--v=production#--v=debug#" \
+        -e "s#REPLACE_IMAGE#registry:5000/nmstate/kubernetes-nmstate-handler#" \
+        $manifests_dir/operator.yaml | $kubectl create -f -
+
+}
+
+function wait_ready() {
+    # Wait until the handler becomes consistently ready on all nodes
+    for ds in nmstate-handler nmstate-handler-worker; do
+        # We have to re-check desired number, sometimes takes some time to be filled in
+        if ! eventually isOk $ds; then
+            echo "Daemon set $ds haven't turned ready within the given timeout"
+            exit 1
+        fi
+
+        # We have to re-check desired number, sometimes takes some time to be filled in
+        if ! consistently isOk $ds; then
+            echo "Daemon set $ds is not consistently ready within the given timeout"
+            exit 1
+        fi
+    done
+}
+
+deploy
+wait_ready
