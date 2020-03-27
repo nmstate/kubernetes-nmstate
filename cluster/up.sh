0aa9875f8026d17e12e7da0a7a0e0a6715fd6e80
Petr Horacek <phoracek@redhat.com>
2020-03-27 21:11:24 +0100

    enable OVS tests again
    Now with the OVS issues fixed on NetworkManager 1.22, we can start testing it
    again.
    
    Signed-off-by: Petr Horacek <phoracek@redhat.com>

--- cluster/up.sh
+++ cluster/up.sh
@@ -12,11 +12,18 @@ if [[ "$KUBEVIRT_PROVIDER" =~ ^(okd|ocp)-.*$$ ]]; then \
 fi
 
 if [[ "$KUBEVIRT_PROVIDER" =~ k8s- ]]; then
+    echo 'Install Open vSwitch on nodes'
+    for node in $(./cluster/kubectl.sh get nodes --no-headers | awk '{print $1}'); do
+        ./cluster/cli.sh ssh ${node} sudo -- yum install -y http://cbs.centos.org/kojifiles/packages/openvswitch/2.9.2/1.el7/x86_64/openvswitch-2.9.2-1.el7.x86_64.rpm http://cbs.centos.org/kojifiles/packages/openvswitch/2.9.2/1.el7/x86_64/openvswitch-devel-2.9.2-1.el7.x86_64.rpm http://cbs.centos.org/kojifiles/packages/dpdk/17.11/3.el7/x86_64/dpdk-17.11-3.el7.x86_64.rpm
+        ./cluster/cli.sh ssh ${node} sudo -- systemctl daemon-reload
+        ./cluster/cli.sh ssh ${node} sudo -- systemctl restart openvswitch
+    done
+
     echo 'Install NetworkManager on nodes'
     for node in $(./cluster/kubectl.sh get nodes --no-headers | awk '{print $1}'); do
         ./cluster/cli.sh ssh ${node} sudo -- yum install -y yum-plugin-copr
         ./cluster/cli.sh ssh ${node} sudo -- yum copr enable -y networkmanager/NetworkManager-1.22
-        ./cluster/cli.sh ssh ${node} sudo -- yum install -y NetworkManager
+        ./cluster/cli.sh ssh ${node} sudo -- yum install -y NetworkManager NetworkManager-ovs
         ./cluster/cli.sh ssh ${node} sudo -- systemctl daemon-reload
         ./cluster/cli.sh ssh ${node} sudo -- systemctl restart NetworkManager
         echo "Check NetworkManager is working fine on node $node"
--- test/e2e/simple_ovs_bridge_test.go
+++ test/e2e/simple_ovs_bridge_test.go
@@ -5,9 +5,7 @@ import (
 	. "github.com/onsi/gomega"
 )
 
-//FIXME: OVS bridge functionality at nmstate is unstable
-//       let's activate this again when that is fix
-var _ = PDescribe("Simple OVS bridge [pending cause nmstate bug -> https://bugzilla.redhat.com/show_bug.cgi?id=1724901]", func() {
+var _ = Describe("Simple OVS bridge", func() {
 	Context("when desiredState is configured with an ovs bridge up", func() {
 		BeforeEach(func() {
 			updateDesiredState(ovsBrUp(bridge1))
