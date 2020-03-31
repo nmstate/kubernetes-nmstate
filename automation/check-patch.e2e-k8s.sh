33d517c9ad58fc2b7aa082eea55c45bc67de1c04
Quique Llorente <ellorent@redhat.com>
2020-03-31 14:17:06 +0200

    automation: Run with 1 master and 2 workers
    The e2e tests run now only at workers and we want to run e2e test at
    multinode so two workers are needed at automation to do so with kubevirtci
    we need KUBEVIRT_NUM_NODES=3
    
    Signed-off-by: Quique Llorente <ellorent@redhat.com>

--- automation/check-patch.e2e-k8s.sh
+++ automation/check-patch.e2e-k8s.sh
@@ -13,7 +13,7 @@ teardown() {
 
 main() {
     export KUBEVIRT_PROVIDER='k8s-1.17.0'
-    export KUBEVIRT_NUM_NODES=2
+    export KUBEVIRT_NUM_NODES=3 # 1 master, 2 workers
     source automation/check-patch.setup.sh
     cd ${TMP_PROJECT_PATH}
 
