845439b1531a2e763ea51dfac06b010eedf3c9a4
Quique Llorente <ellorent@redhat.com>
2020-03-31 13:18:34 +0200

    Use polarion test ids for workflow tests
    Signed-off-by: Quique Llorente <ellorent@redhat.com>

--- test/e2e/openshift.cnv.workflow.sh
+++ test/e2e/openshift.cnv.workflow.sh
@@ -23,8 +23,5 @@ EOF
 fi
 
 # Run workflow tests
-FOCUS_1='Nodes.*when.*are.*up.*and.*new.*interface.*is.*configured.*should.*update.*node.*network.*state.*with.*it'
-FOCUS_2='rollback.*when.*connectivity.*to.*default.*gw.*is.*lost.*after.*state.*configuration.*should.*rollback.*to.*a.*good.*gw.*configuration'
-FOCUS_3='NodeSelector.*when.*policy.*is.*set.*with.*node.*selector.*not.*matching.*any.*nodes.*should.*not.*update.*any.*nodes.*and.*have.*false.*Matching.*state'
-FOCUS_4='EnactmentCondition.*when.*applying.*valid.*config.*should.*go.*from.*Progressing.*to.*Available'
-make test/e2e E2E_TEST_TIMEOUT=${TIMEOUT} E2E_TEST_ARGS="-ginkgo.focus $FOCUS_1|$FOCUS_2|$FOCUS_3|$FOCUS_4" NAMESPACE=$NAMESPACE KUBECONFIG=$KUBECONFIG
+focus='test_id:3796|test_id:3795|test_id:3813|test_id:3794|test_id:3793'
+make test/e2e E2E_TEST_TIMEOUT=${TIMEOUT} E2E_TEST_ARGS="-ginkgo.focus $focus" NAMESPACE=$NAMESPACE KUBECONFIG=$KUBECONFIG
