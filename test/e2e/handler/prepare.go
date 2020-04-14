da9e6e6f143126e4db1400ce2a2114208e642b43
Quique Llorente <ellorent@redhat.com>
2020-04-14 13:55:49 +0200

    Wait at e2e tests for deployment too
    Signed-off-by: Quique Llorente <ellorent@redhat.com>

--- test/e2e/handler/prepare.go
+++ test/e2e/handler/prepare.go
@@ -2,7 +2,6 @@ package handler
 
 import (
 	"fmt"
-	"strings"
 	"testing"
 	"time"
 
@@ -31,7 +30,9 @@ func prepare(t *testing.T) (*framework.TestCtx, string) {
 	namespace, err := ctx.GetNamespace()
 	Expect(err).ToNot(HaveOccurred())
 	err = waitForDaemonSets(t, framework.Global.KubeClient, namespace, time.Second*5, time.Second*90)
-	Expect(err).ToNot(HaveOccurred(), "operator is not ready")
+	Expect(err).ToNot(HaveOccurred(), "operator daemonset is not ready")
+	err = waitForDeployments(t, framework.Global.KubeClient, namespace, time.Second*5, time.Second*90)
+	Expect(err).ToNot(HaveOccurred(), "operator deployment is not ready")
 	return ctx, namespace
 }
 
@@ -40,19 +41,36 @@ func waitForDaemonSets(t *testing.T, kubeclient kubernetes.Interface, namespace 
 		return nil
 	}
 	err := wait.PollImmediate(retryInterval, timeout, func() (done bool, err error) {
-		daemonsets, err := kubeclient.AppsV1().DaemonSets(namespace).List(metav1.ListOptions{})
+		filterByApp := metav1.ListOptions{LabelSelector: "app=kubernetes-nmstate"}
+		daemonsets, err := kubeclient.AppsV1().DaemonSets(namespace).List(filterByApp)
 		if err != nil {
 			return true, errors.Wrapf(err, "failed retrieving daemon sets for namespace %s", namespace)
 		}
 		for _, daemonset := range daemonsets.Items {
-			// we don't have the app=kubernetes-nmstate labels at daemonsets so we
-			// filter by name
-			if !strings.Contains(daemonset.Name, "nmstate") {
-				continue
-			}
 			By(fmt.Sprintf("Checking daemonset %s", daemonset.Name))
 			if daemonset.Status.DesiredNumberScheduled != daemonset.Status.NumberAvailable {
 				return false, nil
+			}
+		}
+		return true, nil
+	})
+	return err
+}
+
+func waitForDeployments(t *testing.T, kubeclient kubernetes.Interface, namespace string, retryInterval, timeout time.Duration) error {
+	if framework.Global.LocalOperator {
+		return nil
+	}
+	err := wait.PollImmediate(retryInterval, timeout, func() (done bool, err error) {
+		filterByApp := metav1.ListOptions{LabelSelector: "app=kubernetes-nmstate"}
+		deployments, err := kubeclient.AppsV1().Deployments(namespace).List(filterByApp)
+		if err != nil {
+			return true, errors.Wrapf(err, "failed retrieving daemon sets for namespace %s", namespace)
+		}
+		for _, deployment := range deployments.Items {
+			By(fmt.Sprintf("Checking deployment %s", deployment.Name))
+			if int(deployment.Status.AvailableReplicas) >= 2 {
+				return true, nil
 			}
 		}
 		return true, nil
