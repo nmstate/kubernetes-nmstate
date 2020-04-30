cf86e5001010eb96f67201c3c0f6cc3f03685317
Brad P. Crochet <brad@redhat.com>
2020-04-30 09:51:26 -0400

    Separate handler and operator test scripts
    Leave the existing scripts in automation/ as handler e2e tests, and
    add scripts for the operator e2e suite. Once these land, add them
    to prow.
    
    Signed-off-by: Brad P. Crochet <brad@redhat.com>

--- automation/check-patch.e2e-ocp.sh
+++ automation/check-patch.e2e-ocp.sh
@@ -27,7 +27,7 @@ main() {
     make cluster-up
     trap teardown EXIT SIGINT SIGTERM SIGSTOP
     make cluster-sync
-    make E2E_TEST_TIMEOUT=2h E2E_TEST_ARGS="-ginkgo.noColor " test-e2e
+    make E2E_TEST_TIMEOUT=2h E2E_TEST_ARGS="-ginkgo.noColor " test-e2e-handler
 }
 
 [[ "${BASH_SOURCE[0]}" == "$0" ]] && main "$@"
--- automation/check-patch.e2e-operator-ocp.sh
+++ automation/check-patch.e2e-operator-ocp.sh
@@ -1,11 +1,11 @@
 #!/bin/bash -xe
 
-# This script should be able to execute functional tests against Kubernetes
-# cluster on any environment with basic dependencies listed in
-# check-patch.packages installed and docker running.
+# This script should be able to execute functional tests against OKD cluster on
+# any environment with basic dependencies listed in check-patch.packages
+# installed and docker running.
 #
 # yum -y install automation/check-patch.packages
-# automation/check-patch.e2e-k8s.sh
+# automation/check-patch.e2e-okd.sh
 
 teardown() {
     make cluster-down
@@ -15,19 +15,19 @@ teardown() {
 }
 
 main() {
-    export KUBEVIRT_PROVIDER='k8s-1.17'
-    export KUBEVIRT_NUM_NODES=3 # 1 master, 2 workers
+    export KUBEVIRT_PROVIDER='ocp-4.4'
+
     source automation/check-patch.setup.sh
     cd ${TMP_PROJECT_PATH}
 
     # Let's fail fast if it's not compiling
-    make handler
+    make operator
 
     make cluster-down
     make cluster-up
     trap teardown EXIT SIGINT SIGTERM SIGSTOP
     make cluster-sync
-    make E2E_TEST_TIMEOUT=1h E2E_TEST_ARGS="-ginkgo.noColor " test-e2e-handler
+    make E2E_TEST_TIMEOUT=2h E2E_TEST_ARGS="-ginkgo.noColor " test-e2e-operator
 }
 
 [[ "${BASH_SOURCE[0]}" == "$0" ]] && main "$@"
