11cce4c3be7ad240bbb7b4d1a7adb562ea51e078
Quique Llorente <ellorent@redhat.com>
2020-05-13 16:51:00 +0200

    Replace 'aktau/github-release' with 'github-release/github-release'
    The repo has change the github organization
    
    Signed-off-by: Quique Llorente <ellorent@redhat.com>

--- tools.go
+++ tools.go
@@ -3,7 +3,7 @@
 package tools
 
 import (
-	_ "github.com/aktau/github-release"
+	_ "github.com/github-release/github-release"
 	_ "github.com/onsi/ginkgo/ginkgo"
 	_ "github.com/operator-framework/operator-sdk/cmd/operator-sdk"
 	_ "k8s.io/kube-openapi/cmd/openapi-gen"
