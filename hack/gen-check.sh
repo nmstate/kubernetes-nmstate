60909749acdd13a43ee4fcb8cff3e198d93bb516
Brad P. Crochet <brad@redhat.com>
2020-05-13 09:10:08 -0400

    Makefile: remove build dependency on generated files
    If generated files are not checked in when they should be, a diff
    between a built image and what is in the repo could occur. This would
    not be desirable. So, remove the Makefile dependency, and add a check
    script that makes sure that what is commited to the repo is the same
    as what should be committed.
    
    Signed-off-by: Brad P. Crochet <brad@redhat.com>

---
+++ hack/gen-check.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+set -xe
+
+if [[ -n "$(git status --porcelain)" ]]; then
+    echo "It seems like you need to run 'make generate'. Please run it and commit the changes"
+    git status --porcelain
+    exit 1
+fi
