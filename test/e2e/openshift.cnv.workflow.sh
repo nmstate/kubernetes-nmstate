691dd3905fdafb45c8d3a02bedd2b9abdef81f25
Enrique Llorente Pastora <ellorent@redhat.com>
2020-05-21 17:21:29 +0200

    Disable tier1 tests ssh strict host check (#544)
    Signed-off-by: Quique Llorente <ellorent@redhat.com>

--- test/e2e/openshift.cnv.workflow.sh
+++ test/e2e/openshift.cnv.workflow.sh
@@ -17,7 +17,7 @@ if [ ! -f  $SSH ]; then
 #!/bin/bash
 node_name=\${1}
 node_ip=\$($KUBECTL get node \${node_name} --no-headers -o wide | awk '{print \$6}')
-ssh core@\${node_ip} -- \${@:3}
+ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null core@\${node_ip} -- \${@:3}
 EOF
     chmod +x ${SSH}
 fi
