ec57f64d305e3654547be435eb8ae89e73482ff5
Quique Llorente <ellorent@redhat.com>
2020-07-21 11:27:32 +0200

    Adapt hack/versions.sh to older pre git 2.22
    Signed-off-by: Quique Llorente <ellorent@redhat.com>

--- hack/versions.sh
+++ hack/versions.sh
@@ -3,6 +3,6 @@
 set -e
 
 git fetch --tags > /dev/null 2>&1|| true
-current_branch=$(git branch --show-current)
+current_branch=$(git rev-parse --abbrev-ref HEAD)
 versions=($(git tag --sort version:refname --merged $current_branch |grep "^v[0-9]*\.[0-9]*\.[0-9]*$"))
 echo ${versions[$1]}
