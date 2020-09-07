4ea4eb92f77a8e11a69a7b92ea777d250fa54463
Radim Hrazdil <rhrazdil@redhat.com>
2020-09-07 15:32:20 +0200

    Refactor nodeSelector,tolerations,affinity template variables
    Refactor nodeSelector,tolerations,affinity template variables
    so that these can be easily modified by cnao.
    Added toYaml helper function to render template structs.
    
    Signed-off-by: Radim Hrazdil <rhrazdil@redhat.com>

---
+++ pkg/render/yaml.go
@@ -0,0 +1,20 @@
+package render
+
+import (
+	"strings"
+
+	"sigs.k8s.io/yaml"
+)
+
+// toYAML takes an interface, marshals it to yaml, and returns a string. It will
+// always return a string, even on marshal error (empty string).
+//
+// This is designed to be called from a template.
+func ToYaml(v interface{}) string {
+	data, err := yaml.Marshal(v)
+	if err != nil {
+		// Swallow errors inside of a template.
+		return ""
+	}
+	return strings.TrimSuffix(string(data), "\n")
+}
