17f865c9b4bf54c9c9caee1f403380d95f56b3ad
Enrique Llorente Pastora <ellorent@redhat.com>
2020-09-17 12:01:26 +0200

    Test nnpc v1alpha1 is still functional (#597)
    Now that the CRDs has being upgraded to v1beta1 we have to be
    sure that we can still populate v1alpha1 version since we
    still support it and it's retrocompatible.
    
    Signed-off-by: Quique Llorente <ellorent@redhat.com>

---
+++ test/e2e/handler/upgrade_test.go
@@ -0,0 +1,39 @@
+package handler
+
+import (
+	"context"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
+	framework "github.com/operator-framework/operator-sdk/pkg/test"
+
+	nmstate "github.com/nmstate/kubernetes-nmstate/pkg/apis/nmstate/shared"
+	nmstatev1alpha1 "github.com/nmstate/kubernetes-nmstate/pkg/apis/nmstate/v1alpha1"
+)
+
+var _ = Describe("NodeNetworkConfigurationPolicy upgrade", func() {
+	Context("when v1alpha1 is populated", func() {
+		BeforeEach(func() {
+			policy := nmstatev1alpha1.NodeNetworkConfigurationPolicy{
+				ObjectMeta: metav1.ObjectMeta{
+					Name: TestPolicy,
+				},
+				Spec: nmstate.NodeNetworkConfigurationPolicySpec{
+					DesiredState: linuxBrUp(bridge1),
+				},
+			}
+			Expect(framework.Global.Client.Create(context.TODO(), &policy, &framework.CleanupOptions{})).To(Succeed(), "should success creating a v1alpha1 nncp")
+		})
+		AfterEach(func() {
+			updateDesiredStateAndWait(linuxBrAbsent(bridge1))
+			resetDesiredStateForNodes()
+		})
+		It("should be stored as v1beta1 and end with available state", func() {
+			waitForAvailableTestPolicy()
+		})
+	})
+
+})
