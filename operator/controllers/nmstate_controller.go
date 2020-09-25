6f960fd595bbea3f3ed51c6f930ad34d01359a36
Quique Llorente <ellorent@redhat.com>
2020-09-25 12:48:15 +0200

    wip
    Signed-off-by: Quique Llorente <ellorent@redhat.com>

--- operator/controllers/nmstate_controller.go
+++ operator/controllers/nmstate_controller.go
@@ -47,9 +47,9 @@ type NMStateReconciler struct {
 	Scheme *runtime.Scheme
 }
 
-// +kubebuilder:rbac:namespace:"{{ .OperatorNamespace}}",groups="",resources=services;endpoints;persistentvolumeclaims;events;configmaps;secrets;pods;,verbs="*"
-// +kubebuilder:rbac:namespace:"{{ .OperatorNamespace}}",groups=apps,resources=deployments;daemonsets;replicasets;statefulsets,verbs="*"
-// +kubebuilder:rbac:namespace:"{{ .OperatorNamespace}}",groups=policy,resources=poddisruptionbudgets,verbs="*"
+// +kubebuilder:rbac:groups="",resources=services;endpoints;persistentvolumeclaims;events;configmaps;secrets;pods,verbs="*",namespace=nmstate
+// +kubebuilder:rbac:groups=apps,resources=deployments;daemonsets;replicasets;statefulsets,verbs="*",namespace=nmstate
+// +kubebuilder:rbac:groups=policy,resources=poddisruptionbudgets,verbs="*",namespace=nmstate
 // +kubebuilder:rbac:groups=admissionregistration.k8s.io,resources=mutatingwebhookconfigurations,verbs="*"
 // +kubebuilder:rbac:groups=rbac.authorization.k8s.io,resources=clusterroles;clusterrolebindings;rolebindings;roles,verbs="*"
 // +kubebuilder:rbac:groups=nmstate.io,resources="*",verbs="*"
