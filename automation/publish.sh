b5bd0882547d582d5c8d96dba74c382226a1db2b
Enrique Llorente Pastora <ellorent@redhat.com>
2020-11-25 12:22:35 +0100

    knmstate, docs: Don't cache builder (#635)
    The current docs builder caching mechanism is implemented as a
    container that use the FROM clausule from the same container that's
    pretty wrong since you get never ending layers, let's just remove that
    cache and think about it if it's necessary, we are talking about mintues.
    
    Signed-off-by: Quique Llorente <ellorent@redhat.com>

--- automation/publish.sh
+++ automation/publish.sh
@@ -8,40 +8,33 @@
 
 image_registry=${IMAGE_REGISTRY:-quay.io}
 image_repo=${IMAGE_REPO:-nmstate}
-branch=${PULL_BASE_REF:-$(git rev-parse --abbrev-ref HEAD)}
-docs_source_branch=master
-docs_container=${image_registry}/${image_repo}/kubernetes-nmstate-docs:$docs_source_branch
 
-# Publish kubernets-nmstate containers
 source automation/check-patch.setup.sh
-(
+cd ${TMP_PROJECT_PATH}
+
+push_knmstate_containers() {
     cd ${TMP_PROJECT_PATH}
     make \
         IMAGE_REGISTRY=${image_registry}  \
         IMAGE_REPO=${image_repo} \
         push-handler \
         push-operator
-)
+}
 
-# We don't have a versioned docs webpage so we only update it from master and
-# not from release branches
-if [ "$branch" == $docs_source_branch ]; then
-
-    # Update kubernetes-nmstate documentation container
-    docker build docs --build-arg BRANCH=$docs_source_branch -t $docs_container
-    docker push $docs_container
-
-    # To pass user/password from automations, idea is taken from [1]
-    # [1] https://stackoverflow.com/questions/8536732/can-i-hold-git-credentials-in-environment-variables
-    git config credential.helper '!f() { sleep 1; echo "username=${GITHUB_USER}"; echo "password=${GITHUB_TOKEN}"; }; f'
-
+publish_docs() {
     # Update gh-pages branch with the generated documentation
-    git checkout gh-pages --quiet
-    git rm -r --quiet *
-    docker run -v $(pwd):/gh-pages $docs_container bash -c "cp -r docs/build/kubernetes-nmstate/* /gh-pages"
-    git add -A
-    git commit -m "updated $(date +"%d.%m.%Y %H:%M:%S")"
-    git push --quiet
+    docker run -v $(pwd)/docs:/docs/ ruby make -C /docs install build
+    rm -rf /tmp/gh-pages
+    git clone --single-branch http://github.com/nmstate/kubernetes-nmstate -b gh-pages /tmp/gh-pages
+    rsync -rt --links --cvs-exclude docs/build/kubernetes-nmstate/* /tmp/gh-pages
+    (
+        cd /tmp/gh-pages
+        git add -A
+        git commit -m "updated $(date +"%d.%m.%Y %H:%M:%S")"
+        git push https://${GITHUB_USER}github.com/nmstate/kubernetes-nmstate gh-pages
+    )
     echo -e "\033[0;32mdemo updated $(date +"%d.%m.%Y %H:%M:%S")\033[0m"
-    git checkout $docs_source_branch --quiet
-fi
+}
+
+push_knmstate_containers
+publish_docs
