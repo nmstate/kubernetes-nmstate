8d86379ed64dd8715bd495e08dca52d488e5eb75
Radim Hrazdil <32546791+rhrazdil@users.noreply.github.com>
2021-06-14 17:18:39 +0200

    Avoid incrementing NRU when NNCE is in progress in (#771)
    At the beginning, check if NNCE exists for a policy and is in
    progressing state.
    
    If it is, then it means that handler was killed while applying the
    desired state - do not increment unavailableNodeCount in such case.
    
    There is still a window where killing handler would cause
    inconsistency - between status is set to Failed or Success and the
    unavailableNodeCount is decremented.
    
    Signed-off-by: Radim Hrazdil <rhrazdil@redhat.com>

--- controllers/nodenetworkconfigurationpolicy_controller_test.go
+++ controllers/nodenetworkconfigurationpolicy_controller_test.go
@@ -1,14 +1,23 @@
 package controllers
 
 import (
+	"context"
+
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/ginkgo/extensions/table"
 	. "github.com/onsi/gomega"
-
+	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/client-go/kubernetes/scheme"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client/fake"
 	"sigs.k8s.io/controller-runtime/pkg/event"
 
+	"github.com/nmstate/kubernetes-nmstate/api/shared"
 	nmstatev1beta1 "github.com/nmstate/kubernetes-nmstate/api/v1beta1"
+	"github.com/nmstate/kubernetes-nmstate/pkg/enactmentstatus/conditions"
 )
 
 var _ = Describe("NodeNetworkConfigurationPolicy controller predicates", func() {
@@ -60,6 +69,123 @@ var _ = Describe("NodeNetworkConfigurationPolicy controller predicates", func() 
 				GenerationNew:   2,
 				ReconcileCreate: true,
 				ReconcileUpdate: true,
+			}),
+	)
+
+	type incrementUnavailableNodeCountCase struct {
+		currentUnavailableNodeCount  int
+		expectedUnavailableNodeCount int
+		expectedReconcileError       string
+		expectedReconcileResult      ctrl.Result
+		previousEnactmentConditions  func(*shared.ConditionList, string)
+		shouldConflict               bool
+	}
+	DescribeTable("when claimNodeRunningUpdate is called and",
+		func(c incrementUnavailableNodeCountCase) {
+			reconciler := NodeNetworkConfigurationPolicyReconciler{}
+			s := scheme.Scheme
+			s.AddKnownTypes(nmstatev1beta1.GroupVersion,
+				&nmstatev1beta1.NodeNetworkConfigurationPolicy{},
+				&nmstatev1beta1.NodeNetworkConfigurationEnactment{},
+				&nmstatev1beta1.NodeNetworkConfigurationEnactmentList{},
+			)
+
+			node := corev1.Node{
+				ObjectMeta: metav1.ObjectMeta{
+					Name: nodeName,
+				},
+			}
+			nncp := nmstatev1beta1.NodeNetworkConfigurationPolicy{
+				ObjectMeta: metav1.ObjectMeta{
+					Name: "test",
+				},
+				Status: shared.NodeNetworkConfigurationPolicyStatus{
+					UnavailableNodeCount: c.currentUnavailableNodeCount,
+				},
+			}
+			nnce := nmstatev1beta1.NodeNetworkConfigurationEnactment{
+				ObjectMeta: metav1.ObjectMeta{
+					Name: shared.EnactmentKey(nodeName, nncp.Name).Name,
+				},
+				Status: shared.NodeNetworkConfigurationEnactmentStatus{},
+			}
+
+			// simulate NNCE existnce/non-existence by setting conditions
+			c.previousEnactmentConditions(&nnce.Status.Conditions, "")
+
+			objs := []runtime.Object{&nncp, &nnce, &node}
+
+			// Create a fake client to mock API calls.
+			clb := fake.ClientBuilder{}
+			clb.WithScheme(s)
+			clb.WithRuntimeObjects(objs...)
+			cl := clb.Build()
+
+			reconciler.Client = cl
+			reconciler.Log = ctrl.Log.WithName("controllers").WithName("NodeNetworkConfigurationPolicy")
+
+			res, err := reconciler.Reconcile(context.TODO(), ctrl.Request{
+				NamespacedName: types.NamespacedName{Name: nncp.Name},
+			})
+
+			if c.shouldConflict {
+				Expect(err.Error()).To(Equal(c.expectedReconcileError))
+			}
+			Expect(res).To(Equal(c.expectedReconcileResult))
+
+			obtainedNNCP := nmstatev1beta1.NodeNetworkConfigurationPolicy{}
+			cl.Get(context.TODO(), types.NamespacedName{Name: nncp.Name}, &obtainedNNCP)
+			Expect(obtainedNNCP.Status.UnavailableNodeCount).To(Equal(c.expectedUnavailableNodeCount))
+		},
+		Entry("No node applying policy with empty enactment, should succeed incrementing UnavailableNodeCount",
+			incrementUnavailableNodeCountCase{
+				currentUnavailableNodeCount:  0,
+				expectedUnavailableNodeCount: 0,
+				previousEnactmentConditions:  func(*shared.ConditionList, string) {},
+				expectedReconcileResult:      ctrl.Result{},
+				shouldConflict:               false,
+			}),
+		Entry("No node applying policy with progressing enactment, should succeed incrementing UnavailableNodeCount",
+			incrementUnavailableNodeCountCase{
+				currentUnavailableNodeCount:  0,
+				expectedUnavailableNodeCount: 0,
+				previousEnactmentConditions:  conditions.SetProgressing,
+				expectedReconcileResult:      ctrl.Result{},
+				shouldConflict:               false,
+			}),
+		Entry("No node applying policy with Pending enactment, should succeed incrementing UnavailableNodeCount",
+			incrementUnavailableNodeCountCase{
+				currentUnavailableNodeCount:  0,
+				expectedUnavailableNodeCount: 0,
+				previousEnactmentConditions:  conditions.SetPending,
+				expectedReconcileResult:      ctrl.Result{},
+				shouldConflict:               false,
+			}),
+		Entry("One node applying policy with empty enactment, should conflict incrementing UnavailableNodeCount",
+			incrementUnavailableNodeCountCase{
+				currentUnavailableNodeCount:  1,
+				expectedUnavailableNodeCount: 1,
+				previousEnactmentConditions:  func(*shared.ConditionList, string) {},
+				expectedReconcileResult:      ctrl.Result{RequeueAfter: nodeRunningUpdateRetryTime},
+				expectedReconcileError:       "Operation cannot be fulfilled on nodenetworkconfigurationpolicies \"test\": maximal number of 1 nodes are already processing policy configuration",
+				shouldConflict:               true,
+			}),
+		Entry("One node applying policy with Progressing enactment, should succeed incrementing UnavailableNodeCount",
+			incrementUnavailableNodeCountCase{
+				currentUnavailableNodeCount:  1,
+				expectedUnavailableNodeCount: 0,
+				previousEnactmentConditions:  conditions.SetProgressing,
+				expectedReconcileResult:      ctrl.Result{},
+				shouldConflict:               false,
+			}),
+		Entry("One node applying policy with Pending enactment, should conflict incrementing UnavailableNodeCount",
+			incrementUnavailableNodeCountCase{
+				currentUnavailableNodeCount:  1,
+				expectedUnavailableNodeCount: 1,
+				previousEnactmentConditions:  conditions.SetPending,
+				expectedReconcileResult:      ctrl.Result{RequeueAfter: nodeRunningUpdateRetryTime},
+				expectedReconcileError:       "Operation cannot be fulfilled on nodenetworkconfigurationpolicies \"test\": maximal number of 1 nodes are already processing policy configuration",
+				shouldConflict:               true,
 			}),
 	)
 })
