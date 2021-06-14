6f1834e44498bfb48e9edc3f423d740ee0b34155
Enrique Llorente Pastora <quique.llorente@gmail.com>
2021-06-14 17:49:34 +0200

    Revert "Don't confict with nodeRunningUpdate same node (#773)" (#774)
    This reverts commit 280de79d44113c23d1ed728e3078a725c9de5466.

--- controllers/nodenetworkconfigurationpolicy_controller_test.go
+++ controllers/nodenetworkconfigurationpolicy_controller_test.go
@@ -1,26 +1,17 @@
 package controllers
 
 import (
-	"context"
-
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/ginkgo/extensions/table"
 	. "github.com/onsi/gomega"
 
-	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/apimachinery/pkg/runtime"
-	"k8s.io/apimachinery/pkg/types"
-	"k8s.io/client-go/kubernetes/scheme"
-	ctrl "sigs.k8s.io/controller-runtime"
-	"sigs.k8s.io/controller-runtime/pkg/client/fake"
 	"sigs.k8s.io/controller-runtime/pkg/event"
 
-	"github.com/nmstate/kubernetes-nmstate/api/shared"
 	nmstatev1beta1 "github.com/nmstate/kubernetes-nmstate/api/v1beta1"
 )
 
-var _ = Describe("NodeNetworkConfigurationPolicy controller", func() {
+var _ = Describe("NodeNetworkConfigurationPolicy controller predicates", func() {
 	type predicateCase struct {
 		GenerationOld   int64
 		GenerationNew   int64
@@ -69,64 +60,6 @@ var _ = Describe("NodeNetworkConfigurationPolicy controller", func() {
 				GenerationNew:   2,
 				ReconcileCreate: true,
 				ReconcileUpdate: true,
-			}),
-	)
-	type claimNodeRunningUpdateCase struct {
-		currentNodeRunningUpdate  string
-		expectedNodeRunningUpdate string
-		shouldConflict            bool
-	}
-	DescribeTable("when claimNodeRunningUpdate is called and",
-		func(c claimNodeRunningUpdateCase) {
-			reconciler := NodeNetworkConfigurationPolicyReconciler{}
-			s := scheme.Scheme
-			s.AddKnownTypes(nmstatev1beta1.GroupVersion,
-				&nmstatev1beta1.NodeNetworkConfigurationPolicy{},
-			)
-
-			nncp := nmstatev1beta1.NodeNetworkConfigurationPolicy{
-				ObjectMeta: metav1.ObjectMeta{
-					Name: "test",
-				},
-				Status: shared.NodeNetworkConfigurationPolicyStatus{
-					NodeRunningUpdate: c.currentNodeRunningUpdate,
-				},
-			}
-
-			objs := []runtime.Object{&nncp}
-
-			// Create a fake client to mock API calls.
-			cl := fake.NewFakeClientWithScheme(s, objs...)
-
-			reconciler.Client = cl
-			reconciler.Log = ctrl.Log.WithName("controllers").WithName("NodeNetworkConfigurationPolicy")
-
-			err := reconciler.claimNodeRunningUpdate(&nncp)
-			if c.shouldConflict {
-				Expect(err).Should(WithTransform(apierrors.IsConflict, BeTrue()), "should conflict")
-			} else {
-				Expect(err).ToNot(HaveOccurred())
-			}
-			obtainedNNCP := nmstatev1beta1.NodeNetworkConfigurationPolicy{}
-			cl.Get(context.TODO(), types.NamespacedName{Name: nncp.Name}, &obtainedNNCP)
-			Expect(obtainedNNCP.Status.NodeRunningUpdate).To(Equal(c.expectedNodeRunningUpdate))
-		},
-		Entry("there is no node configuring network, should update nodeRunnigUpdate field",
-			claimNodeRunningUpdateCase{
-				expectedNodeRunningUpdate: nodeName,
-				shouldConflict:            false,
-			}),
-		Entry("there is different node configuring network, should fail with conflict",
-			claimNodeRunningUpdateCase{
-				currentNodeRunningUpdate:  nodeName + "foo",
-				expectedNodeRunningUpdate: nodeName + "foo",
-				shouldConflict:            true,
-			}),
-		Entry("the node running the handler is configuring the network, should not conflict",
-			claimNodeRunningUpdateCase{
-				currentNodeRunningUpdate:  nodeName,
-				expectedNodeRunningUpdate: nodeName,
-				shouldConflict:            false,
 			}),
 	)
 })
