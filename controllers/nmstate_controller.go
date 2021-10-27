3c8d293a46fcf3342fa94322efdf50383976d93e
Enrique Llorente Pastora <ellorent@redhat.com>
2021-10-27 14:10:11 +0200

    [release-0.52] ovs: Mount OVS socket if needed (#860)
    The nmstate OVS functionality depends on the OVS socket to be mounted at
    the handler container but this is only possible if OVS is installed at
    nodes, this change add optional mount to the handler manifests
    controlled by the environment variable ENABLE_OVS on the operator, it
    also sets the env variable at the openshift operator dockerfile to have
    proper OVS functionality there.
    
    cherry-pick #829
    
    Signed-off-by: Quique Llorente <ellorent@redhat.com>

--- controllers/nmstate_controller.go
+++ controllers/nmstate_controller.go
@@ -183,6 +183,8 @@ func (r *NMStateReconciler) applyHandler(instance *nmstatev1beta1.NMState) error
 	data.Data["HandlerNodeSelector"] = amd64AndCRNodeSelector
 	data.Data["HandlerTolerations"] = handlerTolerations
 	data.Data["HandlerAffinity"] = corev1.Affinity{}
+	_, enableOVS := os.LookupEnv("ENABLE_OVS")
+	data.Data["EnableOVS"] = enableOVS
 	// TODO: This is just a place holder to make template renderer happy
 	//       proper variable has to be read from env or CR
 	data.Data["CARotateInterval"] = ""
