53471d6a9ec8a9f490202bbfe58db2056b46183e
Christoph Stäbler <cstabler@redhat.com>
2021-11-18 15:26:22 +0100

    UPSTREAM: <carry>: Skip e2e tests which are not supported on OVN
    Signed-off-by: Christoph Stäbler <cstabler@redhat.com>
    (cherry picked from commit 1d01efbee52f31d740a39a6cbe647f6b3e39226f)
    (cherry picked from commit 364fe41a21ae954b9d73c27852c1787afa137ebb)
    (cherry picked from commit b6c96410d922a11f8416a0eac60ee0021809111f)
    (cherry picked from commit 5f1b9b5840b4a4567c1c897b208b79d8f2881b77)
    (cherry picked from commit 80af39f0eb6f82a278e308136568fd598dd43c93)
    (cherry picked from commit b2626f294d9a3c4b49ccf580595392743b32bbf9)
    (cherry picked from commit 12054ceb799ef0cd15e0933d7021e0c6bf018012)
    (cherry picked from commit c900358808e94c24fd570e80c3b27cc3c02d4e6c)
    Signed-off-by: Douglas Schilling Landgraf <dougsland@redhat.com>
    (cherry picked from commit 71219bd01749d5c7b8ed83c7be2350f7af2e38b4)
    Signed-off-by: Douglas Schilling Landgraf <dougsland@redhat.com>
    (cherry picked from commit 9e6bd010b2956a7a70a3d57fb540bbb25856c955)
    Signed-off-by: Douglas Schilling Landgraf <dougsland@redhat.com>
    (cherry picked from commit 0f8349dd4df094c2fd07e7357abddc6a23f10b2a)
    Signed-off-by: Douglas Schilling Landgraf <dougsland@redhat.com>
    (cherry picked from commit 49529daa90c55cbb49deb2f142664951ad880fc5)
    Signed-off-by: Mat Kowalski <mko@redhat.com>

--- hack/ocp-e2e-tests.sh
+++ hack/ocp-e2e-tests.sh
@@ -19,9 +19,16 @@ export PRIMARY_NIC=enp2s0
 export FIRST_SECONDARY_NIC=enp3s0
 export SECOND_SECONDARY_NIC=enp4s0
 
+SKIPPED_TESTS="user-guide|bridged"
+
 if oc get ns openshift-ovn-kubernetes &> /dev/null; then
     # We are using OVNKubernetes -> use enp1s0 as primary nic
     export PRIMARY_NIC=enp1s0
+    SKIPPED_TESTS+="|NodeNetworkConfigurationPolicy bonding default interface|\
+with ping fail|\
+when connectivity to default gw is lost after state configuration|\
+when name servers are lost after state configuration|\
+when name servers are wrong after state configuration"
 fi
 
 make cluster-sync-operator
@@ -34,4 +41,4 @@ while ! oc get pods -n nmstate | grep handler; do sleep 1; done
 # Then wait for them to be ready
 while oc get pods -n nmstate | grep "0/1"; do sleep 1; done
 # NOTE(bnemec): The test being filtered with "bridged" was re-enabled in 4.8, but seems to be consistently failing on OCP.
-make test-e2e-handler E2E_TEST_ARGS="--skip=user-guide --skip=bridged" E2E_TEST_TIMEOUT=120m
+make test-e2e-handler E2E_TEST_ARGS="--skip=\"${SKIPPED_TESTS}\"" E2E_TEST_TIMEOUT=120h
