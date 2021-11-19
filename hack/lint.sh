aaca893dc677f6338301b219fa4f7de8a2661a2e
Quique Llorente <ellorent@redhat.com>
2021-11-19 14:58:09 +0100

    lint: Integrate golangci-lint
    There is plenty of linters out there for golang, the golangci-lint allow
    to select wich ones and how you want to run.
    
    Signed-off-by: Quique Llorente <ellorent@redhat.com>

---
+++ hack/lint.sh
@@ -0,0 +1,6 @@
+#!/bin/bash -e
+golangci_lint_version=v1.42.1
+if [ ! -f $(go env GOPATH)/bin/golangci-lint ]; then
+    curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b $(go env GOPATH)/bin $golangci_lint_version
+fi
+golangci-lint run
