4070d087b0592d589d212ddc1e5f61b387fefcf6
Michael Filanov <mfilanov@redhat.com>
2021-11-30 09:44:07 +0200

    Add api submodules to lint target
    Signed-off-by: Michael Filanov <mfilanov@redhat.com>

--- hack/lint.sh
+++ hack/lint.sh
@@ -4,3 +4,7 @@ if [ ! -f $(go env GOPATH)/bin/golangci-lint ]; then
     curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b $(go env GOPATH)/bin $golangci_lint_version
 fi
 golangci-lint run
+(
+	cd api
+	golangci-lint run --config ../.golangci.yml
+)
