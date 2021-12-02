c8a9f144637969a4c35cb8474f50857a25fe25ee
Quique Llorente <ellorent@redhat.com>
2021-12-02 15:23:15 +0100

    automation: Remove install go
    The prow jobs has being updated, go toolchain it's pre installed.
    
    Signed-off-by: Quique Llorente <ellorent@redhat.com>

--- automation/check-patch.setup.sh
+++ automation/check-patch.setup.sh
@@ -8,13 +8,10 @@ tmp_dir=/tmp/knmstate/
 rm -rf $tmp_dir
 mkdir -p $tmp_dir
 
-automation/install-go.sh $tmp_dir
-
 export ARCHS="amd64 arm64"
 export TMP_PROJECT_PATH=$tmp_dir/kubernetes-nmstate
 export E2E_LOGS=${TMP_PROJECT_PATH}/test_logs/e2e
 export ARTIFACTS=${ARTIFACTS-$tmp_dir/artifacts}
-export PATH=$tmp_dir/go/bin/:$PATH
 mkdir -p $ARTIFACTS
 
 
