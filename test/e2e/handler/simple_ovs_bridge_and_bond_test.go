3354876c190bbdbf06652b4b9327a8cdbd869c6d
Alona Paz <alkaplan@redhat.com>
2022-01-04 13:45:46 +0200

    test nmpolicy for ovs slb aggregation link (#948)
    * nmpolicy e2e: test ovs-bridge with slb link aggregation port
    
    Signed-off-by: Alona Kaplan <alkaplan@redhat.com>
    
    * nmpolicy e2e: ovs-bridge with link aggregation port and ovs-interface port
    
    Signed-off-by: Alona Kaplan <alkaplan@redhat.com>

--- test/e2e/handler/simple_ovs_bridge_and_bond_test.go
+++ test/e2e/handler/simple_ovs_bridge_and_bond_test.go
@@ -26,7 +26,7 @@ import (
 	nmstate "github.com/nmstate/kubernetes-nmstate/api/shared"
 )
 
-func ovsBrUpLAGEth1AndEth2(bridgeName string, bondName string) nmstate.State {
+func ovsBrUpLAGEth1AndEth2(bridgeName, bondName, port1Name, port2Name string) nmstate.State {
 	return nmstate.NewState(fmt.Sprintf(`interfaces:
   - name: %s
     type: ovs-bridge
@@ -41,10 +41,10 @@ func ovsBrUpLAGEth1AndEth2(bridgeName string, bondName string) nmstate.State {
             port:
               - name: %s
               - name: %s
-`, bridgeName, bondName, firstSecondaryNic, secondSecondaryNic))
+`, bridgeName, bondName, port1Name, port2Name))
 }
 
-func ovsBrUpLAGEth1Eth2WithInternalPort(bridgeName string, internalPortName string, internalPortMac string) nmstate.State {
+func ovsBrUpLAGEth1Eth2WithInternalPort(bridgeName, internalPortName, internalPortMac, port1Name, port2Name string) nmstate.State {
 	return nmstate.NewState(fmt.Sprintf(`interfaces:
   - name: %s
     type: ovs-interface
@@ -66,7 +66,7 @@ func ovsBrUpLAGEth1Eth2WithInternalPort(bridgeName string, internalPortName stri
           - name: %s
           - name: %s
       - name: %s
-`, internalPortName, internalPortMac, bridgeName, firstSecondaryNic, secondSecondaryNic, internalPortName))
+`, internalPortName, internalPortMac, bridgeName, port1Name, port2Name, internalPortName))
 }
 
 func ovsBrUpLinuxBondEth1AndEth2(bridgeName string, bondName string) nmstate.State {
@@ -116,18 +116,7 @@ func ovsBrAndInternalPortAbsent(bridgeName string, internalPortName string) nmst
 
 var _ = Describe("OVS Bridge", func() {
 	Context("when desiredState is updated with ovs-bridge with link aggregation port", func() {
-		BeforeEach(func() {
-			updateDesiredStateAndWait(ovsBrUpLAGEth1AndEth2(bridge1, bond1))
-		})
-		AfterEach(func() {
-			updateDesiredStateAndWait(ovsBrAbsent(bridge1))
-			for _, node := range nodes {
-				interfacesNameForNodeEventually(node).ShouldNot(ContainElement(bridge1))
-			}
-			resetDesiredStateForNodes()
-		})
-		It("should have the ovs-bridge at currentState", func() {
-			By("Verify all required interfaces are present at currentState")
+		verifyInterfaces := func() {
 			for _, node := range nodes {
 				interfacesForNode(node).Should(ContainElement(SatisfyAll(
 					HaveKeyWithValue("name", bridge1),
@@ -135,6 +124,41 @@ var _ = Describe("OVS Bridge", func() {
 					HaveKeyWithValue("state", "up"),
 				)))
 			}
+		}
+
+		AfterEach(func() {
+			updateDesiredStateAndWait(ovsBrAbsent(bridge1))
+			for _, node := range nodes {
+				interfacesNameForNodeEventually(node).ShouldNot(ContainElement(bridge1))
+			}
+			resetDesiredStateForNodes()
+		})
+		Context("without capture", func() {
+			BeforeEach(func() {
+				updateDesiredStateAndWait(ovsBrUpLAGEth1AndEth2(bridge1, bond1, firstSecondaryNic, secondSecondaryNic))
+			})
+
+			It("should have the ovs-bridge at currentState", func() {
+				By("Verify all required interfaces are present at currentState")
+				verifyInterfaces()
+			})
+		})
+		Context("with capture", func() {
+			BeforeEach(func() {
+				capture := map[string]string{
+					"ethernet-ifaces":  `interfaces.type=="ethernet"`,
+					"secondary-ifaces": `capture.ethernet-ifaces | interfaces.state=="down"`,
+				}
+				updateDesiredStateWithCaptureAndWait(
+					ovsBrUpLAGEth1AndEth2(bridge1, bond1, `"{{ capture.secondary-ifaces.interfaces.0.name }}"`, `"{{ capture.secondary-ifaces.interfaces.1.name }}"`),
+					capture)
+				deletePolicy(TestPolicy)
+			})
+
+			It("should have the ovs-bridge at currentState", func() {
+				By("Verify all required interfaces are present at currentState")
+				verifyInterfaces()
+			})
 		})
 	})
 	PContext("when desiredState is updated with ovs-bridge with linux bond as port [BZ: https://bugzilla.redhat.com/show_bug.cgi?id=2005240]", func() {
@@ -172,25 +196,8 @@ var _ = Describe("OVS Bridge", func() {
 			designatedNode string
 			macAddr        = ""
 		)
-		BeforeEach(func() {
-			designatedNode = nodes[0]
 
-			By(fmt.Sprintf("Getting mac address of %s on %s", firstSecondaryNic, designatedNode))
-			macAddr = macAddress(designatedNode, firstSecondaryNic)
-
-			By("Creating policy with desiredState")
-			updateDesiredStateAndWait(ovsBrUpLAGEth1Eth2WithInternalPort(bridge1, ovsPortName, macAddr))
-		})
-		AfterEach(func() {
-			updateDesiredStateAndWait(ovsBrAndInternalPortAbsent(bridge1, ovsPortName))
-			for _, node := range nodes {
-				interfacesNameForNodeEventually(node).ShouldNot(ContainElement(bridge1))
-				interfacesNameForNodeEventually(node).ShouldNot(ContainElement(ovsPortName))
-			}
-			resetDesiredStateForNodes()
-		})
-
-		It("should have the ovs-bridge and internal port at currentState", func() {
+		verifyInterfaces := func() {
 			By("Verify all required interfaces are present at currentState")
 			interfacesForNode(designatedNode).Should(SatisfyAll(
 				ContainElement(SatisfyAll(
@@ -203,6 +210,57 @@ var _ = Describe("OVS Bridge", func() {
 					HaveKeyWithValue("type", "ovs-interface"),
 					HaveKeyWithValue("state", "up"),
 				))))
+		}
+
+		BeforeEach(func() {
+			designatedNode = nodes[0]
+		})
+
+		AfterEach(func() {
+			updateDesiredStateAtNodeAndWait(designatedNode, ovsBrAndInternalPortAbsent(bridge1, ovsPortName))
+			for _, node := range nodes {
+				interfacesNameForNodeEventually(node).ShouldNot(ContainElement(bridge1))
+				interfacesNameForNodeEventually(node).ShouldNot(ContainElement(ovsPortName))
+			}
+			resetDesiredStateForNodes()
+		})
+
+		Context("without capture", func() {
+			BeforeEach(func() {
+				By(fmt.Sprintf("Getting mac address of %s on %s", firstSecondaryNic, designatedNode))
+				macAddr = macAddress(designatedNode, firstSecondaryNic)
+
+				By("Creating policy with desiredState")
+				updateDesiredStateAtNodeAndWait(designatedNode, ovsBrUpLAGEth1Eth2WithInternalPort(bridge1, ovsPortName, macAddr, firstSecondaryNic, secondSecondaryNic))
+			})
+
+			It("should have the ovs-bridge and internal port at currentState", func() {
+				By("Verify all required interfaces are present at currentState")
+				verifyInterfaces()
+			})
+		})
+
+		Context("with capture", func() {
+			BeforeEach(func() {
+				By("Creating policy with desiredState")
+				capture := map[string]string{
+					"first-secondary-iface": fmt.Sprintf(`interfaces.name=="%s"`, firstSecondaryNic),
+					"ethernet-ifaces":       `interfaces.type=="ethernet"`,
+					"secondary-ifaces":      `capture.ethernet-ifaces | interfaces.state=="down"`,
+				}
+
+				macAddr = `"{{ capture.first-secondary-iface.interfaces.0.mac-address }}"`
+				port1 := `"{{ capture.secondary-ifaces.interfaces.0.name }}"`
+				port2 := `"{{ capture.secondary-ifaces.interfaces.1.name }}"`
+
+				updateDesiredStateWithCaptureAtNodeAndWait(designatedNode, ovsBrUpLAGEth1Eth2WithInternalPort(bridge1, ovsPortName, macAddr, port1, port2), capture)
+				deletePolicy(TestPolicy)
+			})
+
+			It("should have the ovs-bridge and internal port at currentState", func() {
+				By("Verify all required interfaces are present at currentState")
+				verifyInterfaces()
+			})
 		})
 	})
 })
