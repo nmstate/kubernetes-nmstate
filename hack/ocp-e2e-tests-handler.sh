516313d1e53e49cd1da1f033fb40174d9cc1d81b
Christoph Stäbler <cstabler@redhat.com>
2022-01-21 14:12:55 +0100

    UPSTREAM: <carry>: Add make target to run operator e2e tests against ocp
    Signed-off-by: Christoph Stäbler <cstabler@redhat.com>
    (cherry picked from commit 6a8591bd76e3daac081d8cf5b131cd26565e1f44)
    (cherry picked from commit ded676917b318effc114eb9258af1e41f56b7a21)
    (cherry picked from commit 5d1c79910cdd8f96ea31c5c12c9997dff682026c)
    (cherry picked from commit 25ab9205c9c994d90de37c9a261d7ae58098cfe4)
    (cherry picked from commit 9b77532a4d2804cd1e5e99db2272b3f7f68fb005)
    (cherry picked from commit 87cce8c6e334a33dfba692e2cadcb0ee55f4009f)
    (cherry picked from commit d7c68f491845d247d750a30f96abcce3e9861d1a)
    Signed-off-by: Douglas Schilling Landgraf <dougsland@redhat.com>
    (cherry picked from commit a43da048d2461b1a10b9cae672fd480d73bd6f9b)
    Signed-off-by: Douglas Schilling Landgraf <dougsland@redhat.com>
    (cherry picked from commit fc78734c343fe367c39c499a2cdae36745c5a76d)
    Signed-off-by: Douglas Schilling Landgraf <dougsland@redhat.com>
    (cherry picked from commit 89d6eaea06f59a3855d60f327a8021157e94de2b)
    Signed-off-by: Douglas Schilling Landgraf <dougsland@redhat.com>
    (cherry picked from commit 7e9d288ead2d1d5026b65eecc57f12dff284f935)
    Signed-off-by: Mat Kowalski <mko@redhat.com>

--- hack/ocp-e2e-tests-handler.sh
+++ hack/ocp-e2e-tests-handler.sh
@@ -11,16 +11,14 @@
 set -ex
 
 export KUBEVIRT_PROVIDER=external
-export IMAGE_BUILDER="${IMAGE_BUILDER:-podman}"
-export DEV_IMAGE_REGISTRY="${DEV_IMAGE_REGISTRY:-quay.io}"
-export KUBEVIRTCI_RUNTIME="${KUBEVIRTCI_RUNTIME:-podman}"
+export IMAGE_BUILDER=podman
+export DEV_IMAGE_REGISTRY=quay.io
+export KUBEVIRTCI_RUNTIME=podman
 export PRIMARY_NIC=enp2s0
 export FIRST_SECONDARY_NIC=enp3s0
 export SECOND_SECONDARY_NIC=enp4s0
-export FLAKE_ATTEMPTS="${FLAKE_ATTEMPTS:-3}"
 
-SKIPPED_TESTS="user-guide|bridged|\
-when desiredState is updated with ovs-bridge with linux bond as port" # https://bugzilla.redhat.com/show_bug.cgi?id=2005240 is not yet fixed in nmstate 1.2
+SKIPPED_TESTS="user-guide|bridged"
 
 if [ "${CI}" == "true" ]; then
     source ${SHARED_DIR}/fix-uid.sh
@@ -32,14 +30,11 @@ fi
 if oc get ns openshift-ovn-kubernetes &> /dev/null; then
     # We are using OVNKubernetes -> use enp1s0 as primary nic
     export PRIMARY_NIC=enp1s0
-    # Some tests don't work correctly in OVNK due to our inability to modify the br-ex interface
     SKIPPED_TESTS+="|NodeNetworkConfigurationPolicy bonding default interface|\
 with ping fail|\
 when connectivity to default gw is lost after state configuration|\
 when name servers are lost after state configuration|\
-when name servers are wrong after state configuration|\
-LLDP configuration with nmpolicy|\
-with capture"
+when name servers are wrong after state configuration"
 fi
 
 make cluster-sync-operator
@@ -50,4 +45,4 @@ while ! oc get pods -n nmstate | grep handler; do sleep 1; done
 # Then wait for them to be ready
 while oc get pods -n nmstate | grep "0/1"; do sleep 1; done
 # NOTE(bnemec): The test being filtered with "bridged" was re-enabled in 4.8, but seems to be consistently failing on OCP.
-make test-e2e-handler E2E_TEST_ARGS="--skip=\"${SKIPPED_TESTS}\" --flake-attempts=${FLAKE_ATTEMPTS}" E2E_TEST_TIMEOUT=4h
+make test-e2e-handler E2E_TEST_ARGS="--skip=\"${SKIPPED_TESTS}\"" E2E_TEST_TIMEOUT=120h
