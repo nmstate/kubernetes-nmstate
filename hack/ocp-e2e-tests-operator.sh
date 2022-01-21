2a1aeed3d1a8ee3363391fdc2bfa342f86421ab6
Christoph Stäbler <cstabler@redhat.com>
2022-01-21 14:12:55 +0100

    UPSTREAM: <carry>: Add make target to run operator e2e tests against ocp
    Signed-off-by: Christoph Stäbler <cstabler@redhat.com>
    (cherry picked from commit 6a8591bd76e3daac081d8cf5b131cd26565e1f44)
    (cherry picked from commit ded676917b318effc114eb9258af1e41f56b7a21)
    (cherry picked from commit 5d1c79910cdd8f96ea31c5c12c9997dff682026c)
    (cherry picked from commit 25ab9205c9c994d90de37c9a261d7ae58098cfe4)
    (cherry picked from commit 9b77532a4d2804cd1e5e99db2272b3f7f68fb005)
    (cherry picked from commit 87cce8c6e334a33dfba692e2cadcb0ee55f4009f)
    (cherry picked from commit d7c68f491845d247d750a30f96abcce3e9861d1a)
    Signed-off-by: Douglas Schilling Landgraf <dougsland@redhat.com>
    (cherry picked from commit a43da048d2461b1a10b9cae672fd480d73bd6f9b)
    Signed-off-by: Douglas Schilling Landgraf <dougsland@redhat.com>
    (cherry picked from commit fc78734c343fe367c39c499a2cdae36745c5a76d)
    Signed-off-by: Douglas Schilling Landgraf <dougsland@redhat.com>
    (cherry picked from commit 89d6eaea06f59a3855d60f327a8021157e94de2b)
    Signed-off-by: Douglas Schilling Landgraf <dougsland@redhat.com>
    (cherry picked from commit 7e9d288ead2d1d5026b65eecc57f12dff284f935)
    Signed-off-by: Mat Kowalski <mko@redhat.com>

---
+++ hack/ocp-e2e-tests-operator.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Make sure to set the IMAGE_REPO env variable to your quay.io username
+# before running this script.
+
+# Additionally, the e2e tests rely on extra nics being configured on the
+# node. If running from dev-scripts, it will be necessary to configure it to
+# deploy the extra nics.
+# See https://github.com/openshift-metal3/dev-scripts/pull/1286 for an example.
+
+set -ex
+
+export KUBEVIRT_PROVIDER=external
+export IMAGE_BUILDER=podman
+export DEV_IMAGE_REGISTRY=quay.io
+export KUBEVIRTCI_RUNTIME=podman
+
+if [ "${CI}" == "true" ]; then
+    source ${SHARED_DIR}/fix-uid.sh
+    export SSH=./hack/ssh-ci.sh
+else
+    export SSH=./hack/ssh.sh
+fi
+
+make cluster-sync-operator
+make test-e2e-operator
