4c32ad40d44b75fe033e10ce8a1e500b95268d77
Christoph Stäbler <cstabler@redhat.com>
2022-02-24 16:41:47 +0100

    UPSTREAM: <carry>: Fix ssh-ci.sh for dualstack environments
    Signed-off-by: Christoph Stäbler <cstabler@redhat.com>
    (cherry picked from commit fa43d2ca16673f58801dddf4a3abcf15a1e59a34)
    (cherry picked from commit 7a9505c18ae1e9db74d9c1f66c29c84588c0c80a)
    (cherry picked from commit f5dce61307e5d0e94c380d012454fe72d4e2dae0)
    (cherry picked from commit 1c7e78a05f59a82e9b7870fbce5b5312ff1948c0)
    (cherry picked from commit e5fe706e5bf5b15a11be30d03592c033dfc2708f)
    (cherry picked from commit 7778c09a900bd318f159498fdc250ba656aa9b7e)
    Signed-off-by: Douglas Schilling Landgraf <dougsland@redhat.com>
    (cherry picked from commit 1a216d3b63bd8d6389f38535932d0dd7bba98523)
    Signed-off-by: Douglas Schilling Landgraf <dougsland@redhat.com>
    (cherry picked from commit a18955880ac802aae475d733a621015827d92ec3)
    Signed-off-by: Douglas Schilling Landgraf <dougsland@redhat.com>
    (cherry picked from commit b78ccfcfe7bcdebbb1dcc896f1f14e09bf24882c)
    Signed-off-by: Douglas Schilling Landgraf <dougsland@redhat.com>

--- hack/ssh-ci.sh
+++ hack/ssh-ci.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 node_name=${1}
-node_ip=$(oc get no ${node_name} -ojsonpath='{.status.addresses[?(.type=="InternalIP")].address}')
+node_ip=$(oc get node ${node_name} --no-headers -o wide | awk '{print $6}')
 IP="$(cat ${SHARED_DIR}/server-ip)"
 SSHOPTS="-o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null"
        
