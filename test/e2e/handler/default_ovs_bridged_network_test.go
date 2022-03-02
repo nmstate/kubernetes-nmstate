56d74e5663f48b0af1dcdcc31bae372b0070b9c0
Radim Hrazdil <32546791+rhrazdil@users.noreply.github.com>
2022-03-02 12:32:24 +0100

    e2e: Don't skip ovs bridge tests (#996)
    It seems that with NetworkManaget 1.36, we can't reproduce the
    issues that made us skip e2e tests for connecting primary interface
    to an ovs switch.
    This commit removes the skip.
    
    Signed-off-by: Radim Hrazdil <rhrazdil@redhat.com>

--- test/e2e/handler/default_ovs_bridged_network_test.go
+++ test/e2e/handler/default_ovs_bridged_network_test.go
@@ -92,9 +92,7 @@ var _ = Describe("NodeNetworkConfigurationPolicy default ovs-bridged network", f
 			macAddr = macAddress(node, primaryNic)
 		})
 
-		PContext(
-			"and ovs bridge on top of the default interface BZ:[https://bugzilla.redhat.com/show_bug.cgi?id=2011879,"+
-				"https://bugzilla.redhat.com/show_bug.cgi?id=2012420]",
+		Context("and ovs bridge on top of the default interface",
 			func() {
 				BeforeEach(func() {
 					Byf("Creating the %s policy", ovsDefaultNetwork)
@@ -154,8 +152,7 @@ var _ = Describe("NodeNetworkConfigurationPolicy default ovs-bridged network", f
 				})
 
 				It("should keep the default IP address after node reboot", func() {
-					err := restartNode(node)
-					Expect(err).ToNot(HaveOccurred())
+					restartNodeWithoutWaiting(node)
 
 					By("Wait for policy re-reconciled after node reboot")
 					waitForPolicyTransitionUpdate(ovsDefaultNetwork)
