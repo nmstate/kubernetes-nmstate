8559ed3bb1f207ae85ad607954fb85f8dc69385f
Radim Hrazdil <32546791+rhrazdil@users.noreply.github.com>
2022-03-11 14:08:17 +0100

    Add affintiy to NMState CR (#1003)
    Signed-off-by: Radim Hrazdil <rhrazdil@redhat.com>

--- controllers/operator/nmstate_controller.go
+++ controllers/operator/nmstate_controller.go
@@ -185,6 +185,10 @@ func (r *NMStateReconciler) applyHandler(instance *nmstatev1.NMState) error {
 	if handlerTolerations == nil {
 		handlerTolerations = []corev1.Toleration{operatorExistsToleration}
 	}
+	handlerAffinity := instance.Spec.Affinity
+	if handlerAffinity == nil {
+		handlerAffinity = &corev1.Affinity{}
+	}
 
 	const (
 		webhookMinReplicas int32 = 1
@@ -201,6 +205,10 @@ func (r *NMStateReconciler) applyHandler(instance *nmstatev1.NMState) error {
 	if infraTolerations == nil {
 		infraTolerations = []corev1.Toleration{masterExistsNoScheduleToleration}
 	}
+	infraAffinity := instance.Spec.InfraAffinity
+	if infraAffinity == nil {
+		infraAffinity = &corev1.Affinity{}
+	}
 
 	data.Data["HandlerNamespace"] = os.Getenv("HANDLER_NAMESPACE")
 	data.Data["HandlerImage"] = os.Getenv("RELATED_IMAGE_HANDLER_IMAGE")
@@ -208,12 +216,12 @@ func (r *NMStateReconciler) applyHandler(instance *nmstatev1.NMState) error {
 	data.Data["HandlerPrefix"] = os.Getenv("HANDLER_PREFIX")
 	data.Data["InfraNodeSelector"] = archAndCRInfraNodeSelector
 	data.Data["InfraTolerations"] = infraTolerations
-	data.Data["WebhookAffinity"] = corev1.Affinity{}
+	data.Data["WebhookAffinity"] = infraAffinity
 	data.Data["WebhookReplicas"] = webhookReplicas
 	data.Data["WebhookMinReplicas"] = webhookMinReplicas
 	data.Data["HandlerNodeSelector"] = archAndCRNodeSelector
 	data.Data["HandlerTolerations"] = handlerTolerations
-	data.Data["HandlerAffinity"] = corev1.Affinity{}
+	data.Data["HandlerAffinity"] = handlerAffinity
 	// TODO: This is just a place holder to make template renderer happy
 	//       proper variable has to be read from env or CR
 	data.Data["CARotateInterval"] = ""
