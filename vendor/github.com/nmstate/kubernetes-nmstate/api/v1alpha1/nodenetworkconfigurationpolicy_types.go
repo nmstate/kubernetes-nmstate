6cf99847c01dd6187b056f0bdde685299b2a9182
Radim Hrazdil <rhrazdil@redhat.com>
2022-03-28 14:27:41 +0200

    make vendor
    Signed-off-by: Radim Hrazdil <rhrazdil@redhat.com>

--- vendor/github.com/nmstate/kubernetes-nmstate/api/v1alpha1/nodenetworkconfigurationpolicy_types.go
+++ vendor/github.com/nmstate/kubernetes-nmstate/api/v1alpha1/nodenetworkconfigurationpolicy_types.go
@@ -22,6 +22,7 @@ type NodeNetworkConfigurationPolicyList struct {
 // +kubebuilder:subresource:status
 // +kubebuilder:resource:path=nodenetworkconfigurationpolicies,shortName=nncp,scope=Cluster
 // +kubebuilder:printcolumn:name="Status",type="string",JSONPath=".status.conditions[?(@.status==\"True\")].type",description="Status"
+// +kubebuilder:printcolumn:name="Reason",type="string",JSONPath=".status.conditions[?(@.status==\"True\")].reason",description="Reason"
 // +kubebuilder:deprecatedversion
 
 // NodeNetworkConfigurationPolicy is the Schema for the nodenetworkconfigurationpolicies API
