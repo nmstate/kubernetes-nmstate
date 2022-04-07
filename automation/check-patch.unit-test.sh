04108d27ede1a7f40f147fef096493e4432c076e
Enrique Llorente <ellorent@redhat.com>
2022-04-07 14:11:47 +0200

    automation: Install nmstate-devel for unit-test
    Signed-off-by: Enrique Llorente <ellorent@redhat.com>

--- automation/check-patch.unit-test.sh
+++ automation/check-patch.unit-test.sh
@@ -7,8 +7,15 @@
 # yum -y install automation/check-patch.packages
 # automation/check-patch.e2e-k8s.sh
 
+install_nmstate_devel() {
+  dnf install -b -y dnf-plugins-core
+  dnf copr enable -y nmstate/nmstate-git
+  dnf install -b -y nmstate-devel
+}
+
 main() {
     source automation/check-patch.setup.sh
+    sudo install_nmstate_devel
     cd ${TMP_PROJECT_PATH}
     make all
     make UNIT_TEST_ARGS="--output-dir=$ARTIFACTS --no-color --compilers=2" test/unit
