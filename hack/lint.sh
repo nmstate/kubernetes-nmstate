e828290647d515278204aec7ef28e84916455e4c
Enrique Llorente <ellorent@redhat.com>
2022-04-07 12:27:49 +0200

    actions: Remove lint
    We have found weird issues with the actions and the feature it has of
    showing the line at the code review is not used much. This change
    move the lint to "check" target so it's run as part of prow unit-test.
    
    Signed-off-by: Enrique Llorente <ellorent@redhat.com>

--- hack/lint.sh
+++ hack/lint.sh
@@ -3,8 +3,8 @@ golangci_lint_version=v1.42.1
 if [ ! -f $(go env GOPATH)/bin/golangci-lint ]; then
     curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b $(go env GOPATH)/bin $golangci_lint_version
 fi
-golangci-lint run
+golangci-lint run --timeout 5m0s
 (
 	cd api
-	golangci-lint run --config ../.golangci.yml
+	golangci-lint run --timeout 5m0s --config ../.golangci.yml
 )
