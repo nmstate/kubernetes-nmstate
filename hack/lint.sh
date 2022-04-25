a5cfd6160339c3083143d5a6793426fc2fcf5a50
Radim Hrazdil <rhrazdil@redhat.com>
2022-04-25 14:45:22 +0200

    unit,lint: bump timeout to 10m0s
    Signed-off-by: Radim Hrazdil <rhrazdil@redhat.com>

--- hack/lint.sh
+++ hack/lint.sh
@@ -3,8 +3,8 @@ golangci_lint_version=v1.42.1
 if [ ! -f $(go env GOPATH)/bin/golangci-lint ]; then
     curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b $(go env GOPATH)/bin $golangci_lint_version
 fi
-golangci-lint run --timeout 5m0s
+golangci-lint run --timeout 10m0s
 (
 	cd api
-	golangci-lint run --timeout 5m0s --config ../.golangci.yml
+	golangci-lint run --timeout 10m0s --config ../.golangci.yml
 )
