ff01c8b8f88a8ab5d29bb926da6611108b222ff3
Radim Hrazdil <rhrazdil@redhat.com>
2022-04-27 13:12:55 +0200

    e2e,upgrade: use examples from test/doc package
    Signed-off-by: Radim Hrazdil <rhrazdil@redhat.com>

--- test/e2e/upgrade/upgrade_test.go
+++ test/e2e/upgrade/upgrade_test.go
@@ -30,17 +30,10 @@ import (
 	nmstate "github.com/nmstate/kubernetes-nmstate/api/shared"
 	nmstatev1 "github.com/nmstate/kubernetes-nmstate/api/v1"
 	"github.com/nmstate/kubernetes-nmstate/test/cmd"
+	"github.com/nmstate/kubernetes-nmstate/test/doc"
 	"github.com/nmstate/kubernetes-nmstate/test/e2e/operator"
 	testenv "github.com/nmstate/kubernetes-nmstate/test/env"
 )
-
-type upgradePolicyCase struct {
-	name         string
-	fileName     string
-	policyName   string
-	cleanupState *nmstate.State
-	ifaceNames   []string
-}
 
 var _ = Describe("Upgrade", func() {
 	interfaceAbsent := func(iface string) nmstate.State {
@@ -50,117 +43,32 @@ var _ = Describe("Upgrade", func() {
 `, iface))
 	}
 
-	cleanDNSDesiredState := nmstate.NewState(`dns-resolver:
-  config:
-    search: []
-    server: []
-interfaces:
-- name: eth1
-	state: absent
-`)
-
 	kubectlAndCheck := func(command ...string) {
 		out, err := cmd.Kubectl(command...)
 		Expect(err).ShouldNot(HaveOccurred(), out)
 	}
 
-	examples := []upgradePolicyCase{
-		{
-			name:       "Ethernet",
-			fileName:   "ethernet.yaml",
-			policyName: "ethernet",
-			ifaceNames: []string{"eth1"},
-		},
-		{
-			name:       "Linux bridge",
-			fileName:   "linux-bridge.yaml",
-			policyName: "linux-bridge",
-			ifaceNames: []string{"br1"},
-		},
-		{
-			name:       "Linux bridge with custom vlan",
-			fileName:   "linux-bridge-vlan.yaml",
-			policyName: "linux-bridge-vlan",
-			ifaceNames: []string{"br1"},
-		},
-		{
-			name:       "OVS bridge with interface",
-			fileName:   "ovs-bridge-iface.yaml",
-			policyName: "ovs-bridge-iface",
-			ifaceNames: []string{"ovs0", "ovs-bridge", "eth1"},
-		},
-		{
-			name:       "Linux bonding",
-			fileName:   "bond.yaml",
-			policyName: "bond",
-			ifaceNames: []string{"bond0"},
-		},
-		{
-			name:       "Linux bonding and VLAN",
-			fileName:   "bond-vlan.yaml",
-			policyName: "bond-vlan",
-			ifaceNames: []string{"bond0.102", "bond0"},
-		},
-		{
-			name:       "VLAN",
-			fileName:   "vlan.yaml",
-			policyName: "vlan",
-			ifaceNames: []string{"eth1.102", "eth1"},
-		},
-		{
-			name:       "DHCP",
-			fileName:   "dhcp.yaml",
-			policyName: "dhcp",
-			ifaceNames: []string{"eth1"},
-		},
-		{
-			name:       "Static IP",
-			fileName:   "static-ip.yaml",
-			policyName: "static-ip",
-			ifaceNames: []string{"eth1"},
-		},
-		{
-			name:       "Route",
-			fileName:   "route.yaml",
-			policyName: "route",
-			ifaceNames: []string{"eth1"},
-		},
-		{
-			name:         "DNS",
-			fileName:     "dns.yaml",
-			policyName:   "dns",
-			ifaceNames:   []string{},
-			cleanupState: &cleanDNSDesiredState,
-		},
-		{
-			name:       "Worker selector",
-			fileName:   "worker-selector.yaml",
-			policyName: "worker-selector",
-			ifaceNames: []string{"eth1"},
-		},
-	}
-
-	createUpgradeCasePolicy := func(example upgradePolicyCase) {
-		By(fmt.Sprintf("Creating policy %s", example.policyName))
-		kubectlAndCheck("apply", "-f", fmt.Sprintf("test/e2e/upgrade/examples/%s", example.fileName))
+	createUpgradeCasePolicy := func(example doc.ExampleSpec) {
+		By(fmt.Sprintf("Creating policy %s", example.PolicyName))
+		kubectlAndCheck("apply", "-f", fmt.Sprintf("test/e2e/upgrade/examples/%s", example.FileName))
 		By("Waiting for policy to be available")
-		kubectlAndCheck("wait", "nncp", example.policyName, "--for", "condition=Available", "--timeout", "3m")
+		kubectlAndCheck("wait", "nncp", example.PolicyName, "--for", "condition=Available", "--timeout", "3m")
 	}
 
-	createUpgradeCaseCleanupPolicy := func(example upgradePolicyCase) {
-		if example.cleanupState != nil {
-			setDesiredStateWithPolicyEventually(example.policyName, *example.cleanupState)
+	createUpgradeCaseCleanupPolicy := func(example doc.ExampleSpec) {
+		if example.CleanupState != nil {
+			setDesiredStateWithPolicyEventually(example.PolicyName, *example.CleanupState)
 		}
-		if len(example.ifaceNames) > 0 {
-			for _, ifaceName := range example.ifaceNames {
+		if len(example.IfaceNames) > 0 {
+			for _, ifaceName := range example.IfaceNames {
 				setDesiredStateWithPolicyEventually(
-					example.policyName,
+					example.PolicyName,
 					interfaceAbsent(ifaceName),
 				)
 			}
 		}
 
-		kubectlAndCheck("wait", "nncp", example.policyName, "--for", "condition=Available", "--timeout", "3m")
+		kubectlAndCheck("wait", "nncp", example.PolicyName, "--for", "condition=Available", "--timeout", "3m")
 	}
 
 	BeforeEach(func() {
@@ -170,14 +78,14 @@ interfaces:
 	})
 
 	Context("With examples", func() {
-		for _, e := range examples {
+		for _, e := range doc.ExampleSpecs() {
 			example := e
 
-			Context(example.name, func() {
+			Context(example.Name, func() {
 				It("should succeed applying the policy", func() {
 					//TODO: remove when no longer required
 					for _, policyToSkip := range []string{"vlan", "linux-bridge-vlan", "dns"} {
-						if policyToSkip == example.policyName {
+						if policyToSkip == example.PolicyName {
 							Skip("Skipping due to malformed example manifest")
 						}
 					}
@@ -225,13 +133,13 @@ interfaces:
 					}, ReadTimeout, ReadInterval).Should(Succeed())
 
 					By("Wait for policy to be Available again")
-					kubectlAndCheck("wait", "nncp", example.policyName, "--for", "condition=Available", "--timeout", "3m")
+					kubectlAndCheck("wait", "nncp", example.PolicyName, "--for", "condition=Available", "--timeout", "3m")
 
 					By("Apply cleanup policy configuration")
 					createUpgradeCaseCleanupPolicy(example)
 
 					By("Delete policy")
-					deletePolicy(example.policyName)
+					deletePolicy(example.PolicyName)
 				})
 			})
 		}
