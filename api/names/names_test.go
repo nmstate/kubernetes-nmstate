a72ffb762dd0778f47ab31d3472c65f94309fb13
Enrique Llorente Pastora <ellorent@redhat.com>
2022-05-03 16:06:04 +0200

    api: Fix linters (#1076)
    Signed-off-by: Enrique Llorente <ellorent@redhat.com>

--- api/names/names_test.go
+++ api/names/names_test.go
@@ -1,3 +1,20 @@
+/*
+Copyright The Kubernetes NMState Authors.
+
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 package names
 
 import (
@@ -46,20 +63,20 @@ var _ = Describe("IncludeRelationshipLabels", func() {
 			labels := IncludeRelationshipLabels(map[string]string{"foo": "bar"})
 
 			Expect(labels["foo"]).To(Equal("bar"))
-			Expect(labels[COMPONENT_LABEL_KEY]).To(Equal(expectedComponentLabel))
-			Expect(labels[PART_OF_LABEL_KEY]).To(Equal(expectedPartOfLabel))
-			Expect(labels[VERSION_LABEL_KEY]).To(Equal(expectedVersionLabel))
-			Expect(labels[MANAGED_BY_LABEL_KEY]).To(Equal(expectedManagedByLabel))
+			Expect(labels[ComponentLabelKey]).To(Equal(expectedComponentLabel))
+			Expect(labels[PartOfLabelKey]).To(Equal(expectedPartOfLabel))
+			Expect(labels[VersionLabelKey]).To(Equal(expectedVersionLabel))
+			Expect(labels[ManagedByLabelKey]).To(Equal(expectedManagedByLabel))
 		})
 
 		It("Should update labels with the right values", func() {
-			labels := IncludeRelationshipLabels(map[string]string{"foo": "bar", VERSION_LABEL_KEY: "old_version"})
+			labels := IncludeRelationshipLabels(map[string]string{"foo": "bar", VersionLabelKey: "old_version"})
 
 			Expect(labels["foo"]).To(Equal("bar"))
-			Expect(labels[COMPONENT_LABEL_KEY]).To(Equal(expectedComponentLabel))
-			Expect(labels[PART_OF_LABEL_KEY]).To(Equal(expectedPartOfLabel))
-			Expect(labels[VERSION_LABEL_KEY]).To(Equal(expectedVersionLabel))
-			Expect(labels[MANAGED_BY_LABEL_KEY]).To(Equal(expectedManagedByLabel))
+			Expect(labels[ComponentLabelKey]).To(Equal(expectedComponentLabel))
+			Expect(labels[PartOfLabelKey]).To(Equal(expectedPartOfLabel))
+			Expect(labels[VersionLabelKey]).To(Equal(expectedVersionLabel))
+			Expect(labels[ManagedByLabelKey]).To(Equal(expectedManagedByLabel))
 		})
 	})
 
@@ -77,13 +94,13 @@ var _ = Describe("IncludeRelationshipLabels", func() {
 		It("Should return labels with the right values", func() {
 			labels := IncludeRelationshipLabels(map[string]string{"foo": "bar"})
 			Expect(labels["foo"]).To(Equal("bar"))
-			Expect(labels[COMPONENT_LABEL_KEY]).To(Equal(expectedComponentLabel))
-			Expect(labels[PART_OF_LABEL_KEY]).To(Equal(expectedPartOfLabel))
+			Expect(labels[ComponentLabelKey]).To(Equal(expectedComponentLabel))
+			Expect(labels[PartOfLabelKey]).To(Equal(expectedPartOfLabel))
 
-			_, found := labels[VERSION_LABEL_KEY]
+			_, found := labels[VersionLabelKey]
 			Expect(found).To(Equal(false))
 
-			_, found = labels[MANAGED_BY_LABEL_KEY]
+			_, found = labels[ManagedByLabelKey]
 			Expect(found).To(Equal(false))
 		})
 	})
--- api/v1alpha1/nodenetworkconfigurationenactment_types.go
+++ api/v1alpha1/nodenetworkconfigurationenactment_types.go
@@ -1,3 +1,20 @@
+/*
+Copyright The Kubernetes NMState Authors.
+
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 package v1alpha1
 
 import (
@@ -32,7 +49,7 @@ type NodeNetworkConfigurationEnactment struct {
 	Status shared.NodeNetworkConfigurationEnactmentStatus `json:"status,omitempty"`
 }
 
-func NewEnactment(nodeName string, policy NodeNetworkConfigurationPolicy) NodeNetworkConfigurationEnactment {
+func NewEnactment(nodeName string, policy *NodeNetworkConfigurationPolicy) NodeNetworkConfigurationEnactment {
 	enactment := NodeNetworkConfigurationEnactment{
 		ObjectMeta: metav1.ObjectMeta{
 			Name: shared.EnactmentKey(nodeName, policy.Name).Name,
