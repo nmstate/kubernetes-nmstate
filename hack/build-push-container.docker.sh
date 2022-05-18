693fe25a638a1cf2fe7405ab13ea70743b4515ee
Or Shoval <oshoval@redhat.com>
2022-05-18 10:54:56 +0300

    multiarch build: Use buildah
    When using podman, use buildah for multi arch build
    
    Signed-off-by: Or Shoval <oshoval@redhat.com>

--- hack/build-push-container.docker.sh
+++ hack/build-push-container.docker.sh
@@ -24,5 +24,5 @@ PUSH=--push
 if [ "$SKIP_PUSH" == "true" ]; then
     PUSH=""
 fi
-docker buildx build --progress plain --platform ${PLATFORM} $@ -t ${IMAGE} $PUSH
+docker buildx build --progress plain --platform ${PLATFORM} . $@ -t ${IMAGE} $PUSH
 
