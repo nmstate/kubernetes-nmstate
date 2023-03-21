4300d98214615f96e9c9b2d287af1acf20a9df8e
Enrique Llorente <ellorent@redhat.com>
2023-03-21 09:13:18 +0100

    kubevirtci: Update KUBEVIRTCI_TAG
    Signed-off-by: Enrique Llorente <ellorent@redhat.com>

--- cluster/kubevirtci.sh
+++ cluster/kubevirtci.sh
@@ -1,5 +1,5 @@
 export KUBEVIRT_PROVIDER=${KUBEVIRT_PROVIDER:-'k8s-1.26-centos9'}
-export KUBEVIRTCI_TAG=2302221824-e1cf770
+export KUBEVIRTCI_TAG=2303201102-ef46217
 
 KUBEVIRTCI_REPO='https://github.com/kubevirt/kubevirtci.git'
 KUBEVIRTCI_PATH="${PWD}/_kubevirtci"
