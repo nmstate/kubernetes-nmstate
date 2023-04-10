5fb0a25594d7f359565ff5ee2400ab00f635256c
Enrique Llorente <ellorent@redhat.com>
2023-04-10 11:56:56 +0200

    build: Install nmstate from c9s
    Main branch was consuming nmstate from copr #2280 to fix a bug. this
    change revert that to consume the delivered version at latest centos 9
    stream.
    
    Signed-off-by: Enrique Llorente <ellorent@redhat.com>

--- build/install-nmstate.distro.sh
+++ build/install-nmstate.distro.sh
@@ -1,7 +1,3 @@
 #!/bin/bash -xe
 
-#dnf install -b -y -x "*alpha*" -x "*beta*" nmstate
-
-dnf install -b -y dnf-plugins-core
-dnf copr enable -y packit/nmstate-nmstate-2280
-dnf install -b -y nmstate
+dnf install -b -y -x "*alpha*" -x "*beta*" nmstate
