7f6218ec2459774d586be6fbe25dc260a3ab8d31
Enrique Llorente <ellorent@redhat.com>
2023-04-18 12:05:32 +0200

    e2e, handler: Don't put interfaces down
    By default NetworkManager try to put interfaces up after reboot and
    users usually don't set interfaces down using nmstate. This change reset
    the tests doing ipv4/ipv6 enabled false instead of setting them down.
    
    Signed-off-by: Enrique Llorente <ellorent@redhat.com>

--- cluster/up.sh
+++ cluster/up.sh
@@ -11,19 +11,6 @@ if [[ "$KUBEVIRT_PROVIDER" =~ ^(okd|ocp)-.*$$ ]]; then \
 		while ! $(KUBECTL) get securitycontextconstraints; do sleep 1; done; \
 fi
 
-for node in $(./cluster/kubectl.sh get nodes --no-headers | awk '{print $1}'); do
-    for nic in $FIRST_SECONDARY_NIC $SECOND_SECONDARY_NIC; do
-	      uuid=$(./cluster/cli.sh ssh $node -- nmcli --fields=device,uuid  c show  |grep $nic|awk '{print $2}')
-	      if [ ! -z "$uuid" ]; then
-        	  echo "$node: Flushing nic $nic"
-        	  ./cluster/cli.sh ssh $node -- sudo nmcli con del $uuid
-	      fi
-    done
-    for nic in $(nmcli --fields NAME,UUID -t con show | grep 'Wired Connection' | awk -F : '{print $2}'); do
-        nmcli con modify $nic match.interface-name "!$FIRST_SECONDARY_NIC, !$SECOND_SECONDARY_NIC";
-    done
-done
-
 echo 'Upgrading NetworkManager and enabling and starting up openvswitch'
 for node in $(./cluster/kubectl.sh get nodes --no-headers | awk '{print $1}'); do
     ./cluster/cli.sh ssh ${node} -- sudo dnf upgrade -y NetworkManager
