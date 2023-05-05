e49059ff71d9ab8de215e42d4e48dca942badbde
Arunagirinadan Sudharshan <sudhan.aruna.97@gmail.com>
2023-05-05 23:19:32 +0530

    api: Updated to print State Age column for NNCE
    Signed-off-by: Arunagirinadan Sudharshan <sudhan.aruna.97@gmail.com>

--- api/v1alpha1/nodenetworkconfigurationenactment_types.go
+++ api/v1alpha1/nodenetworkconfigurationenactment_types.go
@@ -39,6 +39,7 @@ type NodeNetworkConfigurationEnactmentList struct {
 // +kubebuilder:subresource:status
 // +kubebuilder:resource:path=nodenetworkconfigurationenactments,shortName=nnce,scope=Cluster
 // +kubebuilder:printcolumn:name="Status",type="string",JSONPath=".status.conditions[?(@.status==\"True\")].type",description="Status"
+// +kubebuilder:printcolumn:name="Status Age",type="date",JSONPath=".status.conditions[?(@.status==\"True\")].lastTransitionTime",description="Status Age"
 // +kubebuilder:deprecatedversion
 
 // NodeNetworkConfigurationEnactment is the Schema for the nodenetworkconfigurationenactments API
