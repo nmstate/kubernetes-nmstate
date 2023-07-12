839a17aa9023ed8a59e72b285192d4e89e9bb95e
Enrique Llorente <ellorent@redhat.com>
2023-07-12 15:20:59 +0200

    deps: Bump nmpolicy to have inequality operator
    Latest nmpolicy v0.6.0 implements != operator needed for some scenarios
    where we want to check interfaces that are not part of a specific
    "controller".
    
    Signed-off-by: Enrique Llorente <ellorent@redhat.com>

--- vendor/github.com/nmstate/kubernetes-nmstate/api/v1alpha1/nodenetworkconfigurationenactment_types.go
+++ vendor/github.com/nmstate/kubernetes-nmstate/api/v1alpha1/nodenetworkconfigurationenactment_types.go
@@ -39,6 +39,8 @@ type NodeNetworkConfigurationEnactmentList struct {
 // +kubebuilder:subresource:status
 // +kubebuilder:resource:path=nodenetworkconfigurationenactments,shortName=nnce,scope=Cluster
 // +kubebuilder:printcolumn:name="Status",type="string",JSONPath=".status.conditions[?(@.status==\"True\")].type",description="Status"
+//nolint:lll
+// +kubebuilder:printcolumn:name="Status Age",type="date",JSONPath=".status.conditions[?(@.status==\"True\")].lastTransitionTime",description="Status Age"
 // +kubebuilder:deprecatedversion
 
 // NodeNetworkConfigurationEnactment is the Schema for the nodenetworkconfigurationenactments API
