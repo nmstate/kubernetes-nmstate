d72f7870a87bbb65c7996cd368cbcd53699b6732
Mat Kowalski <mko@redhat.com>
2023-07-18 13:38:14 +0200

    UPSTREAM: <carry>: Fix ssh-ci script to use proper env

--- hack/ssh-ci.sh
+++ hack/ssh-ci.sh
@@ -3,5 +3,10 @@ node_name=${1}
 node_ip=$(oc get node ${node_name} --no-headers -o wide | awk '{print $6}')
 IP="$(cat ${SHARED_DIR}/server-ip)"
 SSHOPTS="-o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null"
-       
-ssh ${SSHOPTS} -i ${CLUSTER_PROFILE_DIR}/packet-ssh-key root@${IP} "ssh ${SSHOPTS} core@${node_ip} -- ${@:3}"
+
+# A hack to avoid "Could not create directory '/.ssh'" error because inside container
+# home is directly at root
+export HOME=/root
+SSHKEYINSIDEPACKET="/root/.ssh/id_rsa"
+
+ssh ${SSHOPTS} -i ${CLUSTER_PROFILE_DIR}/packet-ssh-key root@${IP} "ssh ${SSHOPTS} -i ${SSHKEYINSIDEPACKET} core@${node_ip} -- ${@:3}"
