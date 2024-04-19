5d65a71355fd441245b3b71112d511ad79c9e8b2
Mat Kowalski <mko@redhat.com>
2024-04-19 12:59:46 +0200

    metrics: change port used by metrics server
    Currently metrics server uses port `:8080` what has a high chance of
    conflicting with other services. In order to decrease a chance of the
    conflict we are moving to `:8089`.
    
    Signed-off-by: Mat Kowalski <mko@redhat.com>

--- test/e2e/handler/metrics.go
+++ test/e2e/handler/metrics.go
@@ -20,8 +20,6 @@ package handler
 import (
 	"strings"
 
-	"sigs.k8s.io/controller-runtime/pkg/metrics"
-
 	"github.com/nmstate/kubernetes-nmstate/test/cmd"
 	"github.com/nmstate/kubernetes-nmstate/test/runner"
 )
@@ -29,7 +27,7 @@ import (
 func getMetrics(token string) map[string]string {
 	bearer := "Authorization: Bearer " + token
 	return indexMetrics(runner.RunAtMetricsPod("curl", "-s", "-k", "--header",
-		bearer, metrics.DefaultBindAddress, "https://127.0.0.1:8443/metrics"))
+		bearer, ":8089", "https://127.0.0.1:8443/metrics"))
 }
 
 func getPrometheusToken() (string, error) {
