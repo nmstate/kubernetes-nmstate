b4ca812b28e38bc07ca21b23974ad15b239ec0fd
Enrique Llorente Pastora <ellorent@redhat.com>
2024-06-10 16:17:54 +0200

    deps: Pin go to 1.22 (#1247)
    Signed-off-by: Enrique Llorente <ellorent@redhat.com>

--- build/install-go.sh
+++ build/install-go.sh
@@ -3,6 +3,10 @@
 version=$1
 arch=""
 os=linux
+go_mod_version=$1
+
+dnf install -y jq
+full_version=$(curl -s https://go.dev/dl/?mode=json |jq -r .[].version |grep "$go_mod_version")
 
 case $(uname -m) in
     x86_64)  arch="amd64";;
@@ -14,6 +18,6 @@ if [ "$arch" == "" ]; then
     exit 1
 fi
 
-tarball_url="https://go.dev/dl/go${version}.${os}-${arch}.tar.gz"
+tarball_url="https://go.dev/dl/${full_version}.${os}-${arch}.tar.gz"
 
 curl --retry 10 -L $tarball_url | tar -C /usr/local -zxf -
