1773a489a5281050bb0bfc87c6127bf853dbde59
Mat Kowalski <mko@redhat.com>
2025-03-10 16:48:48 +0100

    api: remove v1alpha1
    Signed-off-by: Mat Kowalski <mko@redhat.com>

--- test/e2e/handler/upgrade_test.go
+++ test/e2e/handler/upgrade_test.go
@@ -27,7 +27,6 @@ import (
 	"k8s.io/apimachinery/pkg/util/intstr"
 
 	nmstate "github.com/nmstate/kubernetes-nmstate/api/shared"
-	nmstatev1alpha1 "github.com/nmstate/kubernetes-nmstate/api/v1alpha1"
 	nmstatev1beta1 "github.com/nmstate/kubernetes-nmstate/api/v1beta1"
 	"github.com/nmstate/kubernetes-nmstate/test/e2e/policy"
 
@@ -35,30 +34,6 @@ import (
 )
 
 var _ = Describe("NodeNetworkConfigurationPolicy upgrade", func() {
-	Context("when v1alpha1 is populated", func() {
-		BeforeEach(func() {
-			maxUnavailableIntOrString := intstr.FromString(maxUnavailable)
-			policy := nmstatev1alpha1.NodeNetworkConfigurationPolicy{
-				ObjectMeta: metav1.ObjectMeta{
-					Name: TestPolicy,
-				},
-				Spec: nmstate.NodeNetworkConfigurationPolicySpec{
-					DesiredState:   linuxBrUp(bridge1),
-					NodeSelector:   map[string]string{"node-role.kubernetes.io/worker": ""},
-					MaxUnavailable: &maxUnavailableIntOrString,
-				},
-			}
-			Expect(testenv.Client.Create(context.TODO(), &policy)).To(Succeed(), "should success creating a v1alpha1 nncp")
-		})
-		AfterEach(func() {
-			updateDesiredStateAndWait(linuxBrAbsent(bridge1))
-			resetDesiredStateForNodes()
-		})
-		It("should be stored as v1 and end with available state", func() {
-			policy.WaitForAvailableTestPolicy()
-		})
-	})
-
 	Context("when v1beta1 is populated", func() {
 		BeforeEach(func() {
 			maxUnavailableIntOrString := intstr.FromString(maxUnavailable)
