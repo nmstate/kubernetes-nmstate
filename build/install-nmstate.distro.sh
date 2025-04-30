e8cd292252ba5a35814a67394698d9c70fc4728c
Enrique Llorente <ellorent@redhat.com>
2025-04-30 09:30:49 +0200

    DNM: Install nmstatectl with fix
    Signed-off-by: Enrique Llorente <ellorent@redhat.com>

--- build/install-nmstate.distro.sh
+++ build/install-nmstate.distro.sh
@@ -1,3 +1,4 @@
 #!/bin/bash -xe
 
 dnf install -b -y -x "*alpha*" -x "*beta*" nmstate
+dnf install -y https://people.redhat.com/fge/RHEL-88896/nmstate-2.2.45-0.20250430.2514git7b1ac4b4.el9.x86_64.rpm
