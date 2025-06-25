b3aaf241c773781ed7b31d7b550dfb719222f01c
Enrique Llorente <ellorent@redhat.com>
2025-06-25 14:33:55 +0200

    crd: implement NMState CRD status subresource
    This commit implements comprehensive status reporting for the NMState operator
    with proper condition management and resource reconciliation.
    
    Asisted-by: claude code
    Signed-off-by: Enrique Llorente <ellorent@redhat.com>

---
+++ pkg/apply/apply.go
@@ -0,0 +1,156 @@
+/*
+Copyright The Kubernetes NMState Authors.
+
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package apply
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"github.com/pkg/errors"
+	"k8s.io/apimachinery/pkg/api/equality"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	apimeta "k8s.io/apimachinery/pkg/api/meta"
+	uns "k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"k8s.io/apimachinery/pkg/types"
+	k8sclient "sigs.k8s.io/controller-runtime/pkg/client"
+)
+
+// ApplyObject applies the desired object against the apiserver,
+// merging it with any existing objects if already present.
+func ApplyObject(ctx context.Context, client k8sclient.Client, obj *uns.Unstructured) error {
+	name := obj.GetName()
+	namespace := obj.GetNamespace()
+	if name == "" {
+		return errors.Errorf("object %s has no name", obj.GroupVersionKind().String())
+	}
+	gvk := obj.GroupVersionKind()
+	// used for logging and errors
+	objDesc := fmt.Sprintf("(%s) %s/%s", gvk.String(), namespace, name)
+	log.Printf("reconciling %s", objDesc)
+
+	if err := IsObjectSupported(obj); err != nil {
+		return errors.Wrapf(err, "object %s unsupported", objDesc)
+	}
+
+	// Get existing
+	existing := &uns.Unstructured{}
+	existing.SetGroupVersionKind(gvk)
+	err := client.Get(ctx, types.NamespacedName{Name: obj.GetName(), Namespace: obj.GetNamespace()}, existing)
+
+	if err != nil && apierrors.IsNotFound(err) {
+		log.Printf("does not exist, creating %s", objDesc)
+		err := client.Create(ctx, obj)
+		if err != nil {
+			// Check if this is a CRD that doesn't exist - treat as non-fatal for optional resources like ServiceMonitor
+			if apimeta.IsNoMatchError(err) {
+				log.Printf("CRD does not exist for %s, skipping", objDesc)
+				return nil
+			}
+			return errors.Wrapf(err, "could not create %s", objDesc)
+		}
+		log.Printf("successfully created %s", objDesc)
+		return nil
+	}
+	if err != nil {
+		// Check if this is a CRD that doesn't exist - treat as non-fatal for optional resources like ServiceMonitor
+		if apimeta.IsNoMatchError(err) {
+			log.Printf("CRD does not exist for %s, skipping", objDesc)
+			return nil
+		}
+		return errors.Wrapf(err, "could not retrieve existing %s", objDesc)
+	}
+
+	if isTLSSecret(obj) {
+		log.Printf("Ignoring TLS secret %s at reconcile", obj.GetName())
+		return nil
+	}
+
+	// Merge the desired object with what actually exists
+	if err := MergeObjectForUpdate(existing, obj); err != nil {
+		return errors.Wrapf(err, "could not merge object %s with existing", objDesc)
+	}
+	if !equality.Semantic.DeepEqual(existing, obj) {
+		if err := client.Update(ctx, obj); err != nil {
+			return errors.Wrapf(err, "could not update object %s", objDesc)
+		}
+		log.Print("update was successful")
+	}
+
+	return nil
+}
+
+// DeleteOwnedObject deletes an object in the apiserver
+func DeleteOwnedObject(ctx context.Context, client k8sclient.Client, obj *uns.Unstructured) error {
+	name := obj.GetName()
+	namespace := obj.GetNamespace()
+	if name == "" {
+		return errors.Errorf("object %s has no name", obj.GroupVersionKind().String())
+	}
+
+	gvk := obj.GroupVersionKind()
+	// used for logging and errors
+	objDesc := fmt.Sprintf("(%s) %s/%s", gvk.String(), namespace, name)
+
+	// Get existing
+	existing := &uns.Unstructured{}
+	existing.SetGroupVersionKind(gvk)
+	err := client.Get(ctx, types.NamespacedName{Name: obj.GetName(), Namespace: obj.GetNamespace()}, existing)
+	if err != nil {
+		// Fail only if the error is not one of the followings:
+		// - not found (nothing to do since it's already gone)
+		// - the type does not exist (if the resource type does not exist then there is no resource at all)
+		if !apierrors.IsNotFound(err) && !apimeta.IsNoMatchError(err) {
+			return errors.Wrapf(err, "failed retrieving owned %s", objDesc)
+		}
+		return nil
+	}
+
+	if !nmstateOwns(existing) {
+		return nil
+	}
+	log.Printf("Handling deletion of %s", objDesc)
+	if err := client.Delete(ctx, existing); err != nil {
+		return errors.Wrapf(err, "failed deleting owned %s", objDesc)
+	}
+
+	return nil
+}
+
+func nmstateOwns(obj *uns.Unstructured) bool {
+	owners := obj.GetOwnerReferences()
+	for _, owner := range owners {
+		if owner.Kind == "NMState" {
+			return true
+		}
+	}
+	return false
+}
+
+func isTLSSecret(obj *uns.Unstructured) bool {
+	if obj.GetKind() != "Secret" {
+		return false
+	}
+
+	typez, found, err := uns.NestedString(obj.Object, "type")
+	if err != nil || !found {
+		return false
+	}
+
+	return typez == "kubernetes.io/tls"
+}
