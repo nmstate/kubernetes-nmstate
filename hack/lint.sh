f0d580f32a418a5b18a73306f717f0a28f4593dd
Emilia Desch <emy@redhat.com>
2025-07-15 14:42:27 +0200

    chore: address nitpicks found by AI in #1350
    Signed-off-by: Emilia Desch <emy@redhat.com>

--- hack/lint.sh
+++ hack/lint.sh
@@ -1,14 +1,17 @@
 #!/bin/bash -xe
 architecture=""
 case $(uname -m) in
-    x86_64) architecture="amd64" ;;
-    arm64)    architecture="arm64" ;;
+    x86_64)           architecture="amd64" ;;
+    aarch64|arm64)    architecture="arm64" ;;
+    *)
+                      echo "Unsupported architecture: $(uname -m), must be (x86_64|aarch64|arm64)."
+                      exit 1;
 esac
 
-path_combined="$(uname -s | tr "[:upper:]" "[:lower:]")"-${architecture}
+path_combined="$(uname -s | tr '[:upper:]' '[:lower:]')-${architecture}"
 golangci_lint_version=1.64.7
-golangci_lint_url=https://github.com/golangci/golangci-lint/releases/download/v${golangci_lint_version}/golangci-lint-${golangci_lint_version}-${path_combined}.tar.gz
-golangci_cmd=/tmp/golangci-lint-${golangci_lint_version}-${path_combined}/golangci-lint
+golangci_lint_url="https://github.com/golangci/golangci-lint/releases/download/v${golangci_lint_version}/golangci-lint-${golangci_lint_version}-${path_combined}.tar.gz"
+golangci_cmd="/tmp/golangci-lint-${golangci_lint_version}-${path_combined}/golangci-lint"
 if [ ! -f "${golangci_cmd}" ]; then
     curl -Lk $golangci_lint_url -o /tmp/golangci-lint-${golangci_lint_version}-${path_combined}.tar.gz
     tar -xvzf /tmp/golangci-lint-${golangci_lint_version}-${path_combined}.tar.gz -C /tmp
