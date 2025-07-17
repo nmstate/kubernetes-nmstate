d4ae89b4cda7ccc2b0b94b53631d6a9645e98418
Emilia Desch <emy@redhat.com>
2025-07-17 11:23:38 +0200

    Make lint.sh download and execution compatible with Darwin/arm64 (#1350)
    * make lint.sh download and execution in hack compatible with arm64 darwin machines
    
    Signed-off-by: Emilia Desch <emy@redhat.com>
    
    * chore: address nitpicks found by AI in #1350
    
    Signed-off-by: Emilia Desch <emy@redhat.com>
    
    ---------
    
    Signed-off-by: Emilia Desch <emy@redhat.com>

--- hack/lint.sh
+++ hack/lint.sh
@@ -1,10 +1,20 @@
 #!/bin/bash -xe
+architecture=""
+case $(uname -m) in
+    x86_64)           architecture="amd64" ;;
+    aarch64|arm64)    architecture="arm64" ;;
+    *)
+                      echo "Unsupported architecture: $(uname -m), must be (x86_64|aarch64|arm64)."
+                      exit 1;
+esac
+
+path_combined="$(uname -s | tr '[:upper:]' '[:lower:]')-${architecture}"
 golangci_lint_version=1.64.7
-golangci_lint_url=https://github.com/golangci/golangci-lint/releases/download/v${golangci_lint_version}/golangci-lint-${golangci_lint_version}-linux-amd64.tar.gz
-golangci_cmd=/tmp/golangci-lint-${golangci_lint_version}-linux-amd64/golangci-lint
+golangci_lint_url="https://github.com/golangci/golangci-lint/releases/download/v${golangci_lint_version}/golangci-lint-${golangci_lint_version}-${path_combined}.tar.gz"
+golangci_cmd="/tmp/golangci-lint-${golangci_lint_version}-${path_combined}/golangci-lint"
 if [ ! -f "${golangci_cmd}" ]; then
-    curl -Lk $golangci_lint_url -o /tmp/golangci-lint-${golangci_lint_version}-linux-amd64.tar.gz
-    tar -xvzf /tmp/golangci-lint-${golangci_lint_version}-linux-amd64.tar.gz -C /tmp
+    curl -Lk $golangci_lint_url -o /tmp/golangci-lint-${golangci_lint_version}-${path_combined}.tar.gz
+    tar -xvzf /tmp/golangci-lint-${golangci_lint_version}-${path_combined}.tar.gz -C /tmp
     chmod 755 ${golangci_cmd}
 fi
 ${golangci_cmd} run --timeout 20m0s
