b274fffecc583c207ebb411495b9fd763dad5857
Mat Kowalski <mko@redhat.com>
2026-03-16 16:39:58 +0100

    metrics: add status counter for NNCP and NNCE
    With this PR we are adding the following metrics:
    
    * kubernetes_nmstate_policies_status
    * kubernetes_nmstate_enactments_status
    
    which count respectively NNCPs grouped by their active status condition
    and NNCEs grouped by node and the active status condition.
    
    This is targeted at users who perfer to track status of NNCPs using
    their monitoring stack instead of `kubectl get nncp [...]` CLI.
    
    In order to keep the metric cardinality at the sane level, we are not
    opting out for NNCP/NNCE name in the metric, only the aggregated count.
    
    Signed-off-by: Mat Kowalski <mko@redhat.com>

---
+++ controllers/metrics/nodenetworkconfigurationpolicy_controller.go
@@ -0,0 +1,125 @@
+/*
+Copyright The Kubernetes NMState Authors.
+
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package metrics
+
+import (
+	"context"
+	"fmt"
+	"slices"
+
+	"github.com/go-logr/logr"
+	"github.com/pkg/errors"
+
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/event"
+	"sigs.k8s.io/controller-runtime/pkg/predicate"
+
+	nmstatev1beta1 "github.com/nmstate/kubernetes-nmstate/api/v1beta1"
+	"github.com/nmstate/kubernetes-nmstate/api/shared"
+	"github.com/nmstate/kubernetes-nmstate/pkg/monitoring"
+)
+
+// NodeNetworkConfigurationPolicyReconciler reconciles NNCP objects for status metrics
+type NodeNetworkConfigurationPolicyReconciler struct {
+	client.Client
+	Log    logr.Logger
+	Scheme *runtime.Scheme
+}
+
+func (r *NodeNetworkConfigurationPolicyReconciler) Reconcile(ctx context.Context, request ctrl.Request) (ctrl.Result, error) {
+	log := r.Log.WithValues("metrics.nodenetworkconfigurationpolicy", request.NamespacedName)
+	log.Info("Reconcile")
+
+	if err := r.reportStatistics(ctx); err != nil {
+		return ctrl.Result{}, fmt.Errorf("failed reporting NNCP status statistics: %w", err)
+	}
+
+	return ctrl.Result{}, nil
+}
+
+func (r *NodeNetworkConfigurationPolicyReconciler) SetupWithManager(mgr ctrl.Manager) error {
+	onConditionChange := predicate.Funcs{
+		CreateFunc: func(event.CreateEvent) bool {
+			return true
+		},
+		DeleteFunc: func(event.DeleteEvent) bool {
+			return true
+		},
+		UpdateFunc: func(e event.UpdateEvent) bool {
+			oldNNCP, ok := e.ObjectOld.(*nmstatev1beta1.NodeNetworkConfigurationPolicy)
+			if !ok {
+				return false
+			}
+			newNNCP, ok := e.ObjectNew.(*nmstatev1beta1.NodeNetworkConfigurationPolicy)
+			if !ok {
+				return false
+			}
+			return !slices.Equal(oldNNCP.Status.Conditions, newNNCP.Status.Conditions)
+		},
+		GenericFunc: func(event.GenericEvent) bool {
+			return false
+		},
+	}
+
+	err := ctrl.NewControllerManagedBy(mgr).
+		For(&nmstatev1beta1.NodeNetworkConfigurationPolicy{}).
+		WithEventFilter(onConditionChange).
+		Complete(r)
+	if err != nil {
+		return errors.Wrap(err, "failed to add controller to NNCP status metrics Reconciler")
+	}
+
+	return nil
+}
+
+func (r *NodeNetworkConfigurationPolicyReconciler) reportStatistics(ctx context.Context) error {
+	nncpList := nmstatev1beta1.NodeNetworkConfigurationPolicyList{}
+	if err := r.List(ctx, &nncpList); err != nil {
+		return err
+	}
+
+	counts := make(map[shared.ConditionType]float64)
+	for _, condType := range shared.NodeNetworkConfigurationPolicyConditionTypes {
+		counts[condType] = 0
+	}
+
+	for i := range nncpList.Items {
+		status := activeConditionType(nncpList.Items[i].Status.Conditions)
+		if status != "" {
+			counts[status]++
+		}
+	}
+
+	for condType, count := range counts {
+		monitoring.PolicyStatus.WithLabelValues(string(condType)).Set(count)
+	}
+
+	return nil
+}
+
+func activeConditionType(conditions shared.ConditionList) shared.ConditionType {
+	for _, c := range conditions {
+		if c.Status == corev1.ConditionTrue {
+			return c.Type
+		}
+	}
+	return ""
+}
